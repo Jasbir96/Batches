66e1170a1fe1dabb60f465acdbb4d5947693d9b2
Jasbir <mr.jasbirsingh96@gmail.com>
2021-03-21 11:28:52 +0530

    added upto folder creation

--- PP-Batch-2/3_Webscrapping13_03_2021/activity/main.js
+++ PP-Batch-2/3_Webscrapping13_03_2021/activity/main.js
@@ -1,5 +1,7 @@
 let request = require("request");
 let cheerio = require("cheerio");
+let fs = require("fs");
+let path = require("path");
 let url = "https://github.com/topics";
 request(url, cb);
 function cb(err, response, html) {
@@ -13,7 +15,7 @@ function cb(err, response, html) {
 function extractData(html) {
     let selTool = cheerio.load(html);
     let anchors = selTool
-    (".no-underline.d-flex.flex-column.flex-justify-center");
+        (".no-underline.d-flex.flex-column.flex-justify-center");
     for (let i = 0; i < anchors.length; i++) {
         let link = selTool(anchors[i]).attr("href");
         // console.log(link);
@@ -35,13 +37,30 @@ function getRepoLinks(html) {
     let selTool = cheerio.load(html);
     let topicNameElem = selTool(".h1-mktg");
     let repolinks = selTool("a.text-bold");
-
     console.log(topicNameElem.text());
-
+    let topicName = topicNameElem.text().trim();
+    dirCreater(topicName);
     for (let i = 0; i < 8; i++) {
         let repoPageLink = selTool(repolinks[i]).attr("href");
-        console.log(repoPageLink);
-    }``
+        let repoName = repoPageLink.split("/").pop();
+        repoName = repoName.trim();
+        console.log(repoName);
+        createFile(repoName, topicName);
+        console.log(repoPageLink)
+    }
     console.log("`````````````````````````");
+}
+function dirCreater(topicName) {
+    let pathOfFolder = path.join(__dirname, topicName);
+    if (fs.existsSync(pathOfFolder) == false) {
+        fs.mkdirSync(pathOfFolder);
+    }
+}
 
+function createFile(repoName, topicName) {
+    let pathofFile = path.join(__dirname, topicName, repoName + ".json");
+    if (fs.existsSync(pathofFile) == false) {
+        let createStream = fs.createWriteStream(pathofFile);
+        createStream.end();
+    }
 }
