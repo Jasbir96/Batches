33c2c1b9349732b65c21e1e28f1122ef5a7cfeae
Jasbir <mr.jasbirsingh96@gmail.com>
2021-04-03 13:48:05 +0530

    added upto hk question identification

--- PP-Batch-1/4_Auto_prom_27_03_2021/activity/code.js
+++ PP-Batch-1/4_Auto_prom_27_03_2021/activity/code.js
@@ -1,21 +1,73 @@
-module.export = {
-    q_1:`#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    int freq[101] = {};
-    for (int i = 0; i < n; i++) {
-        int c;
-        cin >> c;
-        freq[c]++;
-    }
+module.exports = {
+    codes: [
+        {
+        qName: "Counting Valleys",
+        soln: `#include <iostream>
+    #include <cstdio>
+    #include <string>
+    #include <sstream> 
+    #include <vector>
+    #include <set>
+    #include <map>
+    #include <queue>
+    #include <stack>
+    #include <cmath>
+    #include <algorithm>
+    #include <cstring>
+    #include <ctime>
+    #include <cassert>
+    using namespace std;
+    #define pb push_back
+    #define mp make_pair
+    #define pii pair<int,int>
+    #define vi vector<int>
+    #define vpii vector<pii>
+    #define SZ(x) ((int)(x.size()))
+    #define fi first
+    #define se second
+    #define FOR(i,n) for(int (i)=0;(i)<(n);++(i))
+    #define FORI(i,n) for(int (i)=1;(i)<=(n);++(i))
+    #define IN(x,y) ((y).find((x))!=(y).end())
+    #define ALL(t) t.begin(),t.end()
+    #define FOREACH(i,t) for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+    #define REP(i,a,b) for(int (i)=(a);(i)<=(b);++i)
+    #define REPD(i,a,b) for(int (i)=(a); (i)>=(b);--i)
+    #define REMAX(a,b) (a)=max((a),(b));
+    #define REMIN(a,b) (a)=min((a),(b));
+    #define DBG cerr << "debug here" << endl;
+    #define DBGV(vari) cerr << #vari<< " = "<< (vari) <<endl;
+    
+    typedef long long ll;
+    
+    const int MINN = 2;
+    const int MAXN = 1e6;
+    
+    int main()
+    {
+        ios_base::sync_with_stdio(0);
+        int n; 
+        cin >> n;
+        assert(n >= MINN && n <= MAXN);
+        string s;
+        cin >> s;
+        assert(s.length() == n);
+        int res = 0;
+        int level = 0;
+        FOR(i, n)
+        {
+            if(s[i] == 'D')
+            {
+                --level;
+            }
+            else if(s[i] == 'U')
+            {
+                ++level;
+                if(level == 0) ++res;
+            }
+        }
+        assert(level == 0);
+        cout << res << endl;
+    }`
+    }]
 
-    int res = 0;
-    for (int i = 0; i <= 100; i++) {
-        res += freq[i] / 2;
-    }
-    cout << res << endl;
-    return 0;
-}`
 }
--- PP-Batch-1/4_Auto_prom_27_03_2021/activity/hk_code_submit.js
+++ PP-Batch-1/4_Auto_prom_27_03_2021/activity/hk_code_submit.js
@@ -1,5 +1,6 @@
 let puppeteer = require("puppeteer");
 let { password, email } = require("../../../secrets");
+let { codes } = require("./code");
 let fs = require("fs");
 let gtab;
 console.log("Before");
@@ -28,16 +29,24 @@ browserPromise
         let passwordWillBeTypedPromise = gtab.type("#input-2", password, { delay: 200 });
         return passwordWillBeTypedPromise;
     }).then(function () {
-        let loginPageWillBeClickedpromise = gtab.click("button[data-analytics='LoginPassword']");
-        loginPageWillBeClickedpromise;
-    })
-    .then(function () {
-        let clickIPKIt = waitAndClick(".card-content h3[title='Interview Preparation Kit']");
+        let loginPageWillBeClickedpromise = gtab.
+        click("button[data-analytics='LoginPassword']");
+        return loginPageWillBeClickedpromise;
+    }).then(function () {
+        let clickIPKIt = waitAndClick
+        (".card-content h3[title='Interview Preparation Kit']");
         return clickIPKIt;
-    })
-    .then(function () {
-        let warmupClick = waitAndClick("a[data-attr1='warmup']");
+    }).then(function () {
+        let warmupClick = 
+        waitAndClick("a[data-attr1='warmup']");
         return warmupClick;
+    }).then(function () {
+        return gtab.url();
+    }).then(function (url) {
+        console.log(url);
+        let questionObj = codes[0];
+        questionSolver(url, questionObj.soln,questionObj.qName);
+
     })
     .catch(function (err) {
         console.log(err);
@@ -45,25 +54,54 @@ browserPromise
 // promise based function -> wait and click
 function waitAndClick(selector) {
     return new Promise(function (resolve, reject) {
-        let selectorWaitPromise =
-            gtab.waitForSelector(selector, { visible: true });
+        let selectorWaitPromise = gtab.waitForSelector(selector, 
+            { visible: true });
         selectorWaitPromise
             .then(function () {
                 let selectorClickPromise = gtab.click(selector);
                 return selectorClickPromise;
             }).then(function () {
                 resolve();
+            }).catch(function () {
+                reject(err);
             })
     })
 }
+function questionSolver(modulepageUrl, code, questionName) {
+    return new Promise(function (resolve, reject) {
+        // page visit 
+        let reachedPageUrlPromise = gtab.goto(modulepageUrl);
+        reachedPageUrlPromise
+            .then(function () {
+                //  page h4 -> mathcing h4 -> click
+                // function will exceute inside the browser
+                function browserconsolerunFn(questionName) {
+                    let allH4Elem = document.querySelectorAll("h4");
+                    let textArr = [];
+                    for (let i = 0; i < allH4Elem.length; i++) {
+                        let myQuestion = allH4Elem[i]
+                        .innerText.split("\n")[0];
+                        textArr.push(myQuestion);
+                    }
+                    let idx = textArr.indexOf(questionName);
+                    // console.log(idx);
+                    // console.log("hello");
+                    allH4Elem[idx].click();
+                }
+                let pageClickPromise = 
+                gtab.evaluate(browserconsolerunFn, questionName);
+                return pageClickPromise;
+            }).then(function(){
+                resolve();
+            }) 
+        // questionName-> appear -> click
+        // read 
+        // copy
+        // paste
+        // submit 
+    })
+}
 
-// function questionSolver() {
-//     return new Promise(function (resolve, reject) {
-//         fs.readFile("")
-
-
-//     })
-// }
 
 
 
