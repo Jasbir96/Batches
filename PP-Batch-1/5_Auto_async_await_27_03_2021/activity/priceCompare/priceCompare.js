c99b6f9b724121997f6b61a4cf2d921af8266bbe
Jasbir <mr.jasbirsingh96@gmail.com>
2021-04-10 14:59:44 +0530

    added upto serial price analyzer

--- PP-Batch-1/5_Auto_async_await_27_03_2021/activity/priceCompare/priceCompare.js
+++ PP-Batch-1/5_Auto_async_await_27_03_2021/activity/priceCompare/priceCompare.js
@@ -1,6 +1,8 @@
 let puppeteer = require("puppeteer");
 let fs = require("fs");
-let links = ["https://www.amazon.in", "https://www.flipkart.com", "https://paytmmall.com/"];
+let links = ["https://www.amazon.in",
+    "https://www.flipkart.com",
+    "https://paytmmall.com/"];
 let pName = process.argv[2];
 
 console.log("Before");
@@ -11,13 +13,95 @@ console.log("Before");
             defaultViewport: null,
             args: ["--start-maximized"]
         });
-        await getListingFromAmazon(links[0], browserInstance,pName);
+        let amazonArr = await getListingFromAmazon(links[0], browserInstance, pName);
+        let flipkartArr = await getListingFromFlipkart(links[1], browserInstance, pName);
+        let paytmArr = await getListingFromPaytm(links[2], browserInstance, pName);
+        console.table(amazonArr);
+        console.table(flipkartArr);
+        console.table(paytmArr);
     } catch (err) {
         console.log(err);
     }
 })();
-
 //  product Name,url of amazon home page
 // output-> top 5 matching product -> price Name print 
 async function getListingFromAmazon(link, browserInstance, pName) {
+    let newPage = await browserInstance.newPage();
+    await newPage.goto(link);
+    await newPage.type("#twotabsearchtextbox", pName);
+    await newPage.click("#nav-search-submit-button");
+    await newPage.waitForSelector(".a-price-whole", { visible: true });
+    function consoleFn(priceSelector, pNameSelector) {
+        let priceArr = document.querySelectorAll(priceSelector);
+        let PName = document.querySelectorAll(pNameSelector);
+        let details = [];
+        for (let i = 0; i < 5; i++) {
+            let price = priceArr[i].innerText;
+            let Name = PName[i].innerText;
+            details.push({
+                price, Name
+            })
+        }
+        return details;
+    }
+    return newPage.evaluate(consoleFn,
+        ".a-price-whole",
+        ".a-size-medium.a-color-base.a-text-normal");
+    // a-price-whole
+    // .a-section.a-spacing-medium .a-size-medium.a-color-base.a-text-normal 
+}
+async function getListingFromFlipkart(link, browserInstance, pName) {
+    let newPage = await browserInstance.newPage();
+    await newPage.goto(link);
+    await newPage.click("._2KpZ6l._2doB4z");
+    await newPage.type("._3704LK", pName);
+    await newPage.click("button[type='submit']");
+    await newPage.waitForSelector("._30jeq3._1_WHN1", { visible: true });
+    await newPage.waitForSelector("._4rR01T", { visible: true });
+    function consoleFn(priceSelector, pNameSelector) {
+        let priceArr = document.querySelectorAll(priceSelector);
+        let PName = document.querySelectorAll(pNameSelector);
+        let details = [];
+        for (let i = 0; i < 5; i++) {
+            let price = priceArr[i].innerText;
+            let Name = PName[i].innerText;
+            details.push({
+                price, Name
+            })
+        }
+        return details;
+    }
+    return newPage.evaluate(consoleFn,
+        "._30jeq3._1_WHN1",
+        "._4rR01T");
+    //    ._4rR01T
+    // ._30jeq3._1_WHN1
+
+}
+async function getListingFromPaytm(link, browserInstance, pName) {
+    let newPage = await browserInstance.newPage();
+    await newPage.goto(link);
+    await newPage.type("#searchInput",pName,{ delay: 200 });
+    await newPage.keyboard.press("Enter",{ delay: 200 });
+    await newPage.keyboard.press("Enter");
+    await newPage.waitForSelector(".UGUy", { visible: true });
+    await newPage.waitForSelector("._1kMS", { visible: true });
+    function consoleFn(priceSelector, pNameSelector) {
+        let priceArr = document.querySelectorAll(priceSelector);
+        let PName = document.querySelectorAll(pNameSelector);
+        let details = [];
+        for (let i = 0; i < 5; i++) {
+            let price = priceArr[i].innerText;
+            let Name = PName[i].innerText;
+            details.push({
+                price, Name
+            })
+        }
+        return details;
+    }
+    return newPage.evaluate(consoleFn,
+        "._1kMS",
+        ".UGUy");
+
+
 }
