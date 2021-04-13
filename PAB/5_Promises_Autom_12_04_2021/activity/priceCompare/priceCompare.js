ea0e5b9bcc8afa5ded4e8029cde8934134a562d8
Jasbir <mr.jasbirsingh96@gmail.com>
2021-04-13 21:22:40 +0530

    added upto amazon price compare

--- PAB/5_Promises_Autom_12_04_2021/activity/priceCompare/priceCompare.js
+++ PAB/5_Promises_Autom_12_04_2021/activity/priceCompare/priceCompare.js
@@ -12,13 +12,36 @@ let cTab;
         let browser = await browserOpenPromise;
         let allTabsArr = await browser.pages();
         cTab = allTabsArr[0];
-        let list = await getListingFromAmazon(links[0]);
-        console.table(list, pName);
-
+        let list = await getListingFromAmazon(links[0], pName);
+        list 
+        console.table(list);
     } catch (err) {
         console.log(err);
     }
 })();
-async function getListingFromAmazon(link) {
-
+async function getListingFromAmazon(link, pName) {
+    // console.log(pName)
+    // #twotabsearchtextbox
+    // #nav-search-submit-button
+    // span>div>div.rush-component
+    // s-include-content-margin s-border-bottom s-latency-cf-section
+    await cTab.goto(link);
+    // await cTab.waitForSelector("#twotabsearchtextbox",{visible :true});
+    await cTab.type("#twotabsearchtextbox", pName, { delay: 200 });
+    await cTab.click("#nav-search-submit-button");
+    await cTab.waitForSelector(".a-size-medium.a-color-base.a-text-normal", { visible: true });
+    return cTab.evaluate(consoleFn, ".a-size-medium.a-color-base.a-text-normal", ".a-price-whole");
+}
+function consoleFn(nameSelector, priceSelector) {
+    let nameElems = document.querySelectorAll(nameSelector);
+    let priceElems = document.querySelectorAll(priceSelector);
+    let listings = [];
+    for (let i = 0; i < 5; i++) {
+        let name = nameElems[i].innerText;
+        let price = priceElems[i].innerText;
+        listings.push({
+            name, price
+        })
+    }
+    return listings;
 }
