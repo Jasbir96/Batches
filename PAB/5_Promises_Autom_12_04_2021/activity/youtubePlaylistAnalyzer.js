3f7020248cca23900d85b4430d177279bcf69611
Jasbir <mr.jasbirsingh96@gmail.com>
2021-04-14 18:55:49 +0530

    prepared template for youtube playlist analyzer

---
+++ PAB/5_Promises_Autom_12_04_2021/activity/youtubePlaylistAnalyzer.js
@@ -0,0 +1,6 @@
+// no of videos,views
+// Average length of videos
+//  Detail of each video -> {name,duration}
+//  average watch time
+// -> pdf fill
+// https://www.youtube.com/playlist?list=PLzkuLC6Yvumv_Rd5apfPRWEcjf9b1JRnq
