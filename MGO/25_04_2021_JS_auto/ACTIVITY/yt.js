9418ea0f75d5196c3f29d14e532279f5b787a66b
Jasbir <mr.jasbirsingh96@gmail.com>
2021-04-29 20:47:51 +0530

    completed upto yt.js

--- MGO/25_04_2021_JS_auto/ACTIVITY/yt.js
+++ MGO/25_04_2021_JS_auto/ACTIVITY/yt.js
@@ -6,7 +6,6 @@
 const puppeteer = require("puppeteer");
 let page;
 let cVideos = 0;
-let target = 100;
 async function fn() {
     try {
         const browser = await puppeteer
@@ -36,36 +35,40 @@ async function fn() {
         console.log("Title", obj.title, " videos ", obj.nfVideos, "views ", obj.nfViews);
         let noOfVideos = obj.nfVideos.split(" ")[0];
         noOfVideos = Number(noOfVideos);
+        let i = 0;
+        // 700
+        //83
+        while ((noOfVideos - cVideos) > 100) {
+            await scrollDown(page);
+            console.log(i);
+            i++;
+        }
+        // await page.waitForNavigation({ waitUntil: "networkidle0" });
+        // 83 
+        await waitTillHTMLRendered(page);
+        await scrollDown();
+        console.log(cVideos);
         let videoSelector = "#video-title";
         let duration = "span.style-scope.ytd-thumbnail-overlay-time-status-renderer";
-        await page.waitForSelector(videoSelector, { visible: true });
-        await page.waitForSelector(duration, { visible: true });
-        await page.evaluate(function () {
-            let durationElems = document.querySelectorAll("span.style-scope.ytd-thumbnail-overlay-time-status-renderer");
-            durationElems[durationElems.length - 1].scrollIntoView();
-            console.log("hello");
-        });
-        // await page.focus(reqElem);
-        // console.log("Hello");
-        // let i = 0;
-        // while (i <= (noOfVideos / 100)) {
-        //     scrollDown();
-        //     i++;
-        // }
-        // #video-title-> video name
-        // span.style-scope.ytd-thumbnail-overlay-time-status-renderer
-        //  end
-        // await page.waitForSelector(videoSelector, { visible: true });
-        // await page.waitForSelector("span.style-scope.ytd-thumbnail-overlay-time-status-renderer", { visible: true });
-        // let titleDurArr = await page.evaluate(getTitleNDuration, videoSelector, duration);
-        // console.table(titleDurArr);
+        // getTitleNDuration(videoSelector, duration);
+        let titleDurArr = await page.evaluate(getTitleNDuration, videoSelector, duration);
+        console.table(titleDurArr);
     } catch (err) {
         console.log(err);
     }
 }
-
-
-
+// scroll
+async function scrollDown() {
+    let length = await page.evaluate(function () {
+        // console.log("scrolled");
+        // -> time scroll
+        let titleElems = document.querySelectorAll("#video-title");
+        titleElems[titleElems.length - 1].scrollIntoView(true);
+        return titleElems.length;
+    });
+    cVideos = length;
+}
+// duration title
 function getTitleNDuration(videoSelector, duration) {
     let titleElementsArr = document.querySelectorAll(videoSelector);
     let durationElementArr = document.querySelectorAll(duration);
@@ -79,4 +82,35 @@ function getTitleNDuration(videoSelector, duration) {
     }
     return titleDurArr;
 }
+//  html wait 
+async function waitTillHTMLRendered(page, timeout = 30000) {
+    const checkDurationMsecs = 1000;
+    const maxChecks = timeout / checkDurationMsecs;
+    let lastHTMLSize = 0;
+    let checkCounts = 1;
+    let countStableSizeIterations = 0;
+    const minStableSizeIterations = 3;
+
+    while (checkCounts++ <= maxChecks) {
+        let html = await page.content();
+        let currentHTMLSize = html.length;
+
+        let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+
+        console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+
+        if (lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize)
+            countStableSizeIterations++;
+        else
+            countStableSizeIterations = 0; //reset the counter
+
+        if (countStableSizeIterations >= minStableSizeIterations) {
+            console.log("Page rendered fully..");
+            break;
+        }
+
+        lastHTMLSize = currentHTMLSize;
+        await page.waitForTimeout(checkDurationMsecs);
+    }
+};
 fn();
