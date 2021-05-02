d37c9c299299767905d9145995957e4851c1ccf9
Jasbir <mr.jasbirsingh96@gmail.com>
2021-05-02 20:55:49 +0530

    added code upto filters

--- MGO/29_04_2021_Browser_Jira/activity/script.js
+++ MGO/29_04_2021_Browser_Jira/activity/script.js
@@ -1,6 +1,7 @@
 
 let filterColor = document.querySelectorAll(".filter");
 let mainContainer = document.querySelector(".main-container");
+let plusBtn = document.querySelector(".plus");
 
 for (let i = 0; i < filterColor.length; i++) {
     filterColor[i].addEventListener("click", function () {
@@ -11,8 +12,15 @@ for (let i = 0; i < filterColor.length; i++) {
         let mainClasses = mainContainer.getAttribute("class");
         let mainCArr = mainClasses.split(" ");
         mainCArr[1] = color;
-         mainClasses = mainCArr.join(" ");
-        mainContainer.setAttribute("class",mainClasses);
-
+        mainClasses = mainCArr.join(" ");
+        mainContainer.setAttribute("class", mainClasses);
     })
 }
+
+plusBtn.addEventListener("click", function () {
+    let ans = prompt("Enter Yout Task");
+    let color = prompt("Color");
+    console.log(ans, " ", color);
+    // console.log(ans);
+})
+
