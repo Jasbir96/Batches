b930c4cea4f084801f9c0deb82efaec9dcc7d260
Jasbir <mr.jasbirsingh96@gmail.com>
2021-05-03 19:01:28 +0530

    added upto color change in Jira

--- PAB/Browser/DOM_Jira/activity/script.js
+++ PAB/Browser/DOM_Jira/activity/script.js
@@ -8,17 +8,17 @@ let colors = ["lightpink", "lightblue", "lightgreen", "black"];
 let flag = false;
 let cColor = colors[colors.length - 1];
 
-for (let i = 0; i < filterOptions.length; i++) {
-    filterOptions[i].addEventListener("click", function () {
-        let arr = filterOptions[i].children;
-        // present classes
-        // console.log(arr[1]);
-        let chclassesArr = arr[0].classList;
+// for (let i = 0; i < filterOptions.length; i++) {
+//     filterOptions[i].addEventListener("click", function () {
+//         let arr = filterOptions[i].children;
+//         // present classes
+//         // console.log(arr[1]);
+//         let chclassesArr = arr[0].classList;
 
-        // console.log(classesArr[1]);
-        mainContainer.style.backgroundColor = chclassesArr[0];
-    });
-}
+//         // console.log(classesArr[1]);
+//         mainContainer.style.backgroundColor = chclassesArr[0];
+//     });
+// }
 addBtn.addEventListener("click", function () {
     if (flag == false) {
         modalContainer.style.display = "flex";
@@ -37,6 +37,7 @@ for (let i = 0; i < modalFilters.length; i++) {
         cColor = modalFilters[i].classList[1];
     })
 }
+// text area 
 descBox.addEventListener("keydown", function (e) {
     if (e.key == "Enter") {
         let task = descBox.value;
@@ -46,6 +47,7 @@ descBox.addEventListener("keydown", function (e) {
         // ticket create 
         createTicket(task, cColor);
         //  clean up 
+        
         cColor = colors[colors.length - 1];
         modalContainer.style.display = "none";
         flag = false;
@@ -64,12 +66,23 @@ function createTicket(task, cColor) {
 
     let ticketContainer = document.createElement("div");
     ticketContainer.setAttribute("class", "ticket-container");
-    ticketContainer.innerHTML = `    <div class="ticket-color ${cColor}"></div>
+    ticketContainer.innerHTML = `<div class="ticket-color ${cColor}"></div>
        <div class="ticket_sub-container">
              <h3 class="ticket-id">#sampleId</h3>
              <div class="ticket-desc">${task}</div>
          </div>`;
     mainContainer.appendChild(ticketContainer);
-
+    let colorStripElement = ticketContainer.querySelector(".ticket-color");
+    handleTicket(colorStripElement);
 }
-
+function handleTicket(colorStripElement) {
+    colorStripElement.addEventListener("click", function () {
+        let classes = colorStripElement.classList;
+        let initColor = classes[1];
+        let idx = colors.indexOf(initColor);
+        let newidx = (idx + 1) % 4;
+        let newColor = colors[newidx];
+        colorStripElement.classList.remove(initColor);
+        colorStripElement.classList.add(newColor);
+    })
+}
