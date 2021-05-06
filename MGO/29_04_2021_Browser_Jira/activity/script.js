582c9f26797f139978f41b56ac4b9bffbc3b3446
Jasbir <mr.jasbirsingh96@gmail.com>
2021-05-06 21:30:36 +0530

    added upto  modal JS

--- MGO/29_04_2021_Browser_Jira/activity/script.js
+++ MGO/29_04_2021_Browser_Jira/activity/script.js
@@ -1,12 +1,17 @@
 
 let filterColor = document.querySelectorAll(".filter");
+// let filterColor = document.querySelectorAll(".modal-color");
 let mainContainer = document.querySelector(".main-container");
+let modalContainer = document.querySelector(".modal_container");
+let taskBox = document.querySelector(".task_box");
 let plusBtn = document.querySelector(".plus");
+let modalFlag = false;
+let iColor = "black";
+
 for (let i = 0; i < filterColor.length; i++) {
     filterColor[i].addEventListener("click", function () {
         let classes = filterColor[i].getAttribute("class");
         // console.log(classes);
-        let strArr = classes.split(" ");
         let color = strArr[1];
         let mainClasses = mainContainer.getAttribute("class");
         let mainCArr = mainClasses.split(" ");
@@ -16,19 +21,26 @@ for (let i = 0; i < filterColor.length; i++) {
     })
 }
 plusBtn.addEventListener("click", function () {
-    let task = prompt("Enter Yout Task");
-    let color = prompt("Color");
-    // console.log(ans, " ", color);
-    if (task != "" && color != "") {
-        let taskContainer = document.createElement("div");
-        taskContainer.setAttribute("class", "ticket_container");
-        taskContainer.innerHTML = `<div class="ticket_color ${color}"></div>
-             <div class="ticket_desc_container">
-                 <div class="ticket_id">#ExampleId</div>
-                 <div class="ticket_desc">${task}</div>
-             </div>`;
-             mainContainer.appendChild(taskContainer);
-    }
-    // console.log(ans);
+    
+    modalContainer.style.display = "flex";
 })
+taskBox.addEventListener("keydown", function (e) {
+    if (e.key == "Enter" && taskBox.value != "") {
+        let taskContainer = document.createElement("div");
+        let task = taskBox.value;
+        taskContainer.setAttribute("class", "ticket_container");
+        taskContainer.innerHTML = `<div class="ticket_color ${iColor}"></div>
+        <div class="ticket_desc_container">
+        <div class="ticket_id">#ExampleId</div>
+        <div class="ticket_desc">${task}</div>
+        </div>`;
+        mainContainer.appendChild(taskContainer);
+        // cleanup code
+        modalContainer.style.display = "none";
+        taskBox.value = "";
+
+    }
+
+})
+
 
