096ba09b25b5e6ebec10497b1b3222b73e073c42
Jasbir <mr.jasbirsingh96@gmail.com>
2021-05-06 22:52:19 +0530

    added upto local storage

--- MGO/29_04_2021_Browser_Jira/activity/script.js
+++ MGO/29_04_2021_Browser_Jira/activity/script.js
@@ -1,13 +1,12 @@
-
 let filterColor = document.querySelectorAll(".filter");
-// let filterColor = document.querySelectorAll(".modal-color");
+let modalColors = document.querySelectorAll(".modal-color");
 let mainContainer = document.querySelector(".main-container");
 let modalContainer = document.querySelector(".modal_container");
 let taskBox = document.querySelector(".task_box");
 let plusBtn = document.querySelector(".plus");
 let modalFlag = false;
 let iColor = "black";
-
+let colors = ["pink", "blue", "green", "black"];
 for (let i = 0; i < filterColor.length; i++) {
     filterColor[i].addEventListener("click", function () {
         let classes = filterColor[i].getAttribute("class");
@@ -21,7 +20,6 @@ for (let i = 0; i < filterColor.length; i++) {
     })
 }
 plusBtn.addEventListener("click", function () {
-    
     modalContainer.style.display = "flex";
 })
 taskBox.addEventListener("keydown", function (e) {
@@ -38,9 +36,36 @@ taskBox.addEventListener("keydown", function (e) {
         // cleanup code
         modalContainer.style.display = "none";
         taskBox.value = "";
-
+        iColor = "black";
+        addFunctionality(taskContainer);
     }
-
 })
+for (let i = 0; i < modalColors.length; i++) {
+    modalColors[i].addEventListener("click", function () {
+        let color = modalColors[i].classList[1];
+        iColor = color;
+        // remove everyone
+        for (let j = 0; j < modalColors.length; j++) {
+            modalColors[j].classList.remove("border");
+        }
+        // add 
+        modalColors[i].classList.add("border");
+
+    })
+}
+function addFunctionality(taskContainer) {
+    let ticketColor = taskContainer.querySelector(".ticket_color");
+    ticketColor.addEventListener("click", function () {
+        let cColor = ticketColor.classList[1];
+        let idx = colors.indexOf(cColor);
+        let newIdx = (idx + 1) % 4;
+        let newColor = colors[newIdx];
+        ticketColor.classList.remove(cColor);
+        ticketColor.classList.add(newColor);
+    })
+
+
+}
+
 
 
--- MGO/29_04_2021_Browser_Jira/raw/todo/script.js
+++ MGO/29_04_2021_Browser_Jira/raw/todo/script.js
@@ -1,9 +1,30 @@
 // alert("Hello from JS")
 ;
-
+let arr = [];
 // get -> first elment 
 let inputBox = document.querySelector(".input-box");
 let ul = document.querySelector(".task-list");
+if (localStorage.getItem("allTask")) {
+    let strArr = localStorage.getItem("allTask");
+    arr = JSON.parse(strArr);
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement("li");
+        // append 
+        ul.appendChild(li);
+        li.textContent = arr[i];
+        li.setAttribute("class", "task");
+        li.addEventListener("dblclick", function delFN() {
+            // li.remove();
+            // console.log("i was dbl clicked",li.innerText)
+            let idx = arr.indexOf(li.innerText);
+            arr.splice(idx, 1);
+            let strArr = JSON.stringify(arr);
+            localStorage.setItem("allTask", strArr);
+            li.remove();
+        });
+
+    }
+}
 // listener
 // document -> form -> value // rest -> textContent
 inputBox.addEventListener("keydown", function (object) {
@@ -17,14 +38,28 @@ inputBox.addEventListener("keydown", function (object) {
         // append 
         ul.appendChild(li);
         li.textContent = value;
+        arr.push(value);
+        let strArr = JSON.stringify(arr);
+        localStorage.setItem("allTask", strArr);
         // set Attribute
         li.setAttribute("class", "task");
         li.addEventListener("dblclick", function delFN() {
             // li.remove();
             // console.log("i was dbl clicked",li.innerText)
             li.remove();
+            // remove localStorage
+            let idx = arr.indexOf(li.innerText);
+            arr.splice(idx, 1);
+            let strArr = JSON.stringify(arr);
+            localStorage.setItem("allTask", strArr);
         });
         inputBox.value = "";
     }
 })
+
+
+// get Item 
+// set Item 
+// clear
+// removeItem
 
