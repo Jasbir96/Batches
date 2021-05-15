19d66471495c69b658d768827c57c3ef8d83319e
Jasbir <mr.jasbirsingh96@gmail.com>
2021-05-15 17:21:45 +0530

    added bold with icons

--- PP-Batch-2/browser/excel/formatting.js
+++ PP-Batch-2/browser/excel/formatting.js
@@ -1,21 +1,56 @@
 let bold = document.querySelector(".fa-bold");
 bold.addEventListener("click", function () {
     // address get -> address
+    // ui elemnt
+    let uiCell = getcell();
+    let { rid, cid } = getRIdIdfromAddress();
+    // console.log(rid,cid);
+    // sheet array
+    let cellObj = sheetArr[rid][cid];
+    if (cellObj.isBold == true) {
+        uiCell.style.fontWeight = "normal";
+        bold.classList.remove("menu-active");
+        cellObj.isBold = false
+    } else {
+        uiCell.style.fontWeight = "bold";
+        bold.classList.add("menu-active");
+        cellObj.isBold = true;
+    }
+})
+function getcell() {
     let address = addressElem.value;
     let { rid, cid } = getRIdIdfromAddress(address);
-    console.log(rid,cid);
-    let cell = document.querySelector(`.grid .cell[rid="${rid}"][ cid="${cid}"]`)
-
-    cell.style.fontWeight = "bold";
-    bold.classList.add("menu-active");
-    // cell -> matching cid ,rid
-    // cell style
-})
-function getRIdIdfromAddress(address) {
+    console.log(rid, cid);
+    return document.querySelector(`.grid .cell[rid="${rid}"][ cid="${cid}"]`)
+}
+function getRIdIdfromAddress() {
+    
+    let address = addressElem.value;
     // A->z
     // 1->100
-    console.log(address)
     let cid = Number(address.charCodeAt(0)) - 65;
     let rid = Number(address.slice(1)) - 1;
     return { cid, rid };
 }
+
+let Allcells = document.querySelectorAll(".grid .cell");
+let addressElem = document.querySelector(".address");
+for (let i = 0; i < Allcells.length; i++) {
+    Allcells[i].addEventListener("click", function () {
+        let cid = Allcells[i].getAttribute("cid");
+        let rid = Allcells[i].getAttribute("rid");
+        cid = Number(cid);
+        rid = Number(rid);
+        // console.log(rid,cid);
+        addressElem.value = `${String.fromCharCode(65 + cid)}${rid + 1}`;
+        let cellObj = sheetArr[rid][cid];
+        if (cellObj.isBold==true) {
+            bold.classList.add("menu-active");
+        }else{
+            bold.classList.remove("menu-active");
+            
+        }
+    }
+    )
+}
+Allcells[0].click();
--- PP-Batch-2/browser/excel/init.js
+++ PP-Batch-2/browser/excel/init.js
@@ -17,34 +17,30 @@ for (let i = 1; i <= rows; i++) {
     div.setAttribute("class", "block");
     leftCol.appendChild(div);
 }
+let sheetArr = [];
 // rows-> 100
 // col - > 26
 for (let i = 0; i < rows; i++) {
     let row = document.createElement("div");
+    let rowArr = [];
     row.setAttribute("class", "row");
     for (let j = 0; j < columns; j++) {
-        let col = document.createElement("div");
-       
-        col.setAttribute("class", "cell");
-        col.setAttribute("rid", i);
-        col.setAttribute("cid", j);
-        col.setAttribute("contenteditable","true");
-
-        row.appendChild(col);
+        // UI
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("rid", i);
+        cell.setAttribute("cid", j);
+        cell.setAttribute("contenteditable", "true");
+        row.appendChild(cell);
+        // 
+        let cellObj = {
+            isBold: false,
+            isItalic:false,
+            isUnderline:false
+        }
+        rowArr.push(cellObj);
     }
     grid.appendChild(row);
+    sheetArr.push(rowArr);
 }
-let Allcells = document.querySelectorAll(".grid .cell");
-let addressElem = document.querySelector(".address");
-for (let i = 0; i < Allcells.length; i++) {
-    Allcells[i].addEventListener("click", function () {
-        let cid = Allcells[i].getAttribute("cid");
-        let rid = Allcells[i].getAttribute("rid");
-        cid=Number(cid);
-        rid=Number(rid);
-        // console.log(rid,cid);
-        addressElem.value=`${String.fromCharCode(65 + cid)}${rid + 1}`; 
-    }
-    )
-}
-Allcells[0].click();
+console.log(sheetArr)
