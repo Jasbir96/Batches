53fb4152629f8bedb880e8847ae0603d1edbd021
Jasbir <mr.jasbirsingh96@gmail.com>
2021-06-14 22:34:33 +0530

    added upto routing example

---
+++ PAB/React/4_React_Movies/movies/src/Routing.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+// npm install react-router-dom
+import { Route } from "react-router-dom";
+export default class Routing extends Component {
+    render() {
+        return (
+            <div>
+                Routing Example
+                {/* path-> / ->home*/}
+                {/* route -> path is a suboath route match */}
+                {/* switch -> every route that i have i will render only one of them */}
+                {/* ui */}
+                <ul>
+                    <li><Link to="/home/profile">Profile</Link> </li>
+                    <li><Link to="/listing">Listing</Link></li>
+                    <li><Link to="/home">Home</Link></li>
+                </ul>
+                {/* /logic */}
+                <Switch>
+                    <Route path="/home/profile" component={Profile}></Route>
+                    <Route path="/listing" component={Listing}></Route>
+                    <Redirect from="/home" to="/"></Redirect>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route component={Error}></Route>
+                </Switch>
+
+            </div>
+        )
+    }
+}
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+} class Profile extends Component {
+    render() {
+        return (
+            <div>
+                Profile
+            </div>
+        )
+    }
+} class Listing extends Component {
+    render() {
+        return (
+            <div>
+                Listing
+            </div>
+        )
+    }
+}
+class Error extends Component {
+    render() {
+        return (
+            <div>
+                Error
+            </div>
+        )
+    }
+}
--- PAB/React/4_React_Movies/movies/src/index.js
+++ PAB/React/4_React_Movies/movies/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routing from './Routing';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <App />
- ,
+  // <App />
+  <BrowserRouter>
+    <Routing></Routing>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
