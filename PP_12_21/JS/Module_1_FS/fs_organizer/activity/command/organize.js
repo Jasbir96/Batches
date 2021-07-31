fa00165239ae3600f6be9cccd8bfdd71f4783900
Jasbir <mr.jasbirsingh96@gmail.com>
2021-07-31 12:23:00 +0530

    added upto fs organizer

--- PP_12_21/JS/Module_1_FS/fs_organizer/activity/command/organize.js
+++ PP_12_21/JS/Module_1_FS/fs_organizer/activity/command/organize.js
@@ -1,16 +1,62 @@
-
+let fs = require("fs");
+let path = require("path");
 let types = {
-    media: ["mp4", "mkv"],
+    media: ["mp4", "mkv", "mp3"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
     app: ['exe', 'dmg', 'pkg', "deb"]
 }
+function organizeFn(srcPath) {
+    if (srcPath == undefined)
+        srcPath = process.cwd();
+    // console.log("organize implemneted", srcPath);
+    // 1 create organized_files -> directory
+    let organizedFilesPath = path.join(srcPath, "organized_files");
+    if (fs.existsSync(organizedFilesPath) == false) {
+        fs.mkdirSync(organizedFilesPath);
+    }
+    // 2 scan whole srcPath 
+    let allTheFiles = fs.readdirSync(srcPath);
+    // console.log(allTheFiles);
+    // 3. extension check -> classify
+    for (let i = 0; i < allTheFiles.length; i++) {
+        let fullOriginalPath = path.join(srcPath, allTheFiles[i]);
+        if (fs.lstatSync(fullOriginalPath).isFile() == true) {
 
-
-function organizeFn(src) {
-    console.log("organize  command executed with path: " + src);
-    // code 
+            let folderName = checkextnTellFolder(allTheFiles[i]);
+            copyFileTOdest(folderName, fullOriginalPath, srcPath);
+        }
+    }
+    //  3. copy to that folder to which it belongs
+    // folder
+    // // file copy 
+    // other
+    // // file copy  
 }
+function copyFileTOdest(folderName, fullOriginalPath, srcPath) {
+    let destFolderPath = path.join(srcPath, "organized_files",folderName);
+    if (fs.existsSync(destFolderPath) == false) {
+        fs.mkdirSync(destFolderPath);
+    }
+    let originalFileName = path.basename(fullOriginalPath);
+    let destFilePath = path.join(destFolderPath, originalFileName)
+    fs.copyFileSync(fullOriginalPath, destFilePath);
+    console.log(originalFileName, "copeid to ", folderName);
+
+}
+function checkextnTellFolder(fileName) {
+    let extName = path.extname(fileName);
+    extName = extName.slice(1);
+    for (let key in types) {
+        for (let i = 0; i < types[key].length; i++) {
+            if (types[key][i] == extName) {
+                return key;
+            }
+        }
+    }
+    return "others";
+}
+
 module.exports = {
     organizefxn: organizeFn
 }
--- PP_12_21/JS/Module_1_FS/fs_organizer/activity/command/tree.js
+++ PP_12_21/JS/Module_1_FS/fs_organizer/activity/command/tree.js
@@ -1,8 +1,25 @@
-function treeFn(src) {
-    console.log("tree command executed with path : " + src);
+let fs = require("fs");
+let path = require("path");
+function treeFn(srcPath) {
+    if (srcPath == undefined)
+        srcPath = process.cwd();
+    // console.log("Tree command", srcPath);
+    let content = fs.readdirSync(srcPath);
+    console.log(content);
+    // └──
+    // ├──
+    let parentFOlderName = path.basename(srcPath);
+    let completePath = "└──" + parentFOlderName;
+    // console.log(completePath);
+ 
+    for (let i = 0; i < content.length; i++) {
+        completePath = completePath + "\n\t" + "├──"+content[i];
+console.log(completePath)
+console.log("`````````````````");
+    }
     
+    console.log(completePath);
 }
-
 module.exports = {
-    treefxn: treeFn
+    treeFn: tree
 }
