da0e37998861832fb90bea2618fb20384e3d722b
Jasbir <mr.jasbirsingh96@gmail.com>
2021-08-03 19:03:18 +0530

    added error and  loader

--- GTBIT_21/reels/src/firebaseAuthPOC/Login.jsx
+++ GTBIT_21/reels/src/firebaseAuthPOC/Login.jsx
@@ -5,35 +5,46 @@ const auth = firebase.auth();
 function Login() {
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
+    let [userId, setuserId] = useState("");
+    let [loginLoder, setLoginLoader] = useState(false);
+    let [error, setError] = useState("");
     const loginFn = async () => {
-        // email,password -> firebase 
-        // *******firebase console
-        // auth start
-        // enable -> email and password
-        // user create on firebase side 
-
-        // npm i firebase ` -> local machine 
-        // firebase login
-        let res = await auth.signInWithEmailAndPassword(email, password);
-        // unique id 
-        console.log(res.user.uid);
+        try {
+            setLoginLoader(true);
+            let res = await auth.signInWithEmailAndPassword(email, password);
+            // unique id 
+            console.log(res.user.uid);
+            setLoginLoader(false)
+            setuserId(res.user.uid)
+        } catch (err) {
+            setLoginLoader(false);
+            setError(err.message);
+            setTimeout(() => {
+                setError(false);
+            }, 2000);
+        }
     }
     return (
-        <div >
-            <div> Email:
-                <input type="Email" placeholder="Your Email" value={email} 
-                onChange={function (e) {
-                    // logicsetEmail(e.target.value)
-                }} />
-            </div>
-            <div>Password:
-                <input type="password" placeholder="********" value={password} onChange={(e) => {
-                    setPassword(e.target.value)
-                }}
-                />
-            </div>
-            <button onClick={loginFn}>Login</button>
-        </div>
+        // form will available
+        loginLoder == true ? <h1>Loading....</h1> 
+        :userId ? <h1>{userId}</h1> 
+        : <div>
+    <div> Email:
+        <input type="Email" placeholder="Your Email" value={email}
+                                onChange={function (e) {
+                                    // logic
+                                    setEmail(e.target.value)
+                                }} />
+                        </div>
+                        <div>Password:
+     <input type="password" placeholder="********" value={password} onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}
+                            />
+                        </div>
+                        <button onClick={loginFn}>Login</button>
+                        {error ? <h1>{error}</h1>:<></> }
+                    </div >
     )
 }
 export default Login
