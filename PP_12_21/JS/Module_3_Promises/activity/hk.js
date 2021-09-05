c9b3e9ced512da17ba4c86c58e1c8e9720b40ae2
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-05 12:16:14 +0530

    added upto n running code

--- PP_12_21/JS/Module_3_Promises/activity/hk.js
+++ PP_12_21/JS/Module_3_Promises/activity/hk.js
@@ -1,7 +1,7 @@
 const loginLink = "https://www.hackerrank.com/auth/login";
 const emailpassObj = require("./secrets");
 const { answers } = require("./codes");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer")
 // creates headless browser
 let browserStartPromise = puppeteer.launch({
     // visible 
@@ -31,22 +31,33 @@ browserStartPromise
         let passwordWillBeEnteredPromise = page.type("input[type='password']", emailpassObj.password, { delay: 50 });
         return passwordWillBeEnteredPromise;
     }).then(function () {
-        let loginWillBeDOnepromise = page.click('button[data-analytics="LoginPassword"]', { delay: 100 });
+        let loginWillBeDOnepromise =
+            page.click('button[data-analytics="LoginPassword"]', { delay: 100 });
         return loginWillBeDOnepromise;
     })
     .then(function () {
-        let clickedOnAlgoPromise = waitAndClick(".track-card a[data-attr2='algorithms']", page);
+        let clickedOnAlgoPromise =
+            waitAndClick(".track-card a[data-attr2='algorithms']", page);
         return clickedOnAlgoPromise;
     }).then(function () {
-        let getToWarmUp = waitAndClick("input[value='warmup']", page);
+        let getToWarmUp =
+            waitAndClick("input[value='warmup']", page);
         return getToWarmUp;
     }).then(function () {
         let waitFor3SecondsPromise = page.waitFor(3000);
         return waitFor3SecondsPromise;
     }).then(function () {
-        let challengeClickPromise = page.click(".challenge-submit-btn", { delay: 100 });
-        return challengeClickPromise;``
-
+        let AllChallengesArrPromise =
+            page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled"
+                , { delay: 100 });
+        return AllChallengesArrPromise;
+    }).then(function (questionsArr) {
+        // n number of question first 
+        console.log("number of questions", questionsArr.length);
+        let qWillBeSolvedPromise = questionSolver(page, questionsArr[0], answers[0]);
+        return qWillBeSolvedPromise;
+    }).then(function () {
+        console.log("question is solved");
     })
 function waitAndClick(selector, cPage) {
     return new Promise(function (resolve, reject) {
@@ -64,3 +75,77 @@ function waitAndClick(selector, cPage) {
     }
     )
 }
+
+// return a promise -> that will submit a given question 
+function questionSolver(page, question, answer) {
+    return new Promise(function (resolve, reject) {
+        let qWillBeCLickedPromise = question.click();
+
+        qWillBeCLickedPromise
+            //click
+            // code type 
+            // ctrl A+ ctrl x
+            // click on editor 
+            // ctrl A+ctrl v
+            //  reached to editor
+            .then(function () {
+                // focus 
+                let waitFOrEditorToBeInFocus =
+                    waitAndClick(".monaco-editor.no-user-select.vs", page);
+                return waitFOrEditorToBeInFocus;
+            })
+            // click
+            .then(function () {
+                return waitAndClick(".checkbox-input", page);
+            }).then(function () {
+     return page.waitForSelector("textarea.custominput", { visible: true });
+            })
+            .then(function () {
+                return page.type("textarea.custominput", answer, { delay: 10 });
+            }).then(function () {
+                let ctrlIsPressedP = page.keyboard.down("Control");
+                return ctrlIsPressedP;
+            }).then(function () {
+                let AIsPressedP = page.keyboard.press("A", { delay: 100 });
+                return AIsPressedP;
+            }).then(function () {
+                return page.keyboard.press("X", { delay: 100 });
+            }).then(function () {
+                let ctrlIsPressedP = page.keyboard.up("Control");
+                return ctrlIsPressedP;
+            })
+            .then(function () {
+                // focus 
+                let waitFOrEditorToBeInFocus =
+                    waitAndClick(".monaco-editor.no-user-select.vs", page);
+                return waitFOrEditorToBeInFocus;
+            })
+            .then(function () {
+                let ctrlIsPressedP = page.keyboard.down("Control");
+                return ctrlIsPressedP;
+            }).then(function () {
+                let AIsPressedP = page.keyboard.press("A", { delay: 100 });
+                return AIsPressedP;
+            }).then(function () {
+                let AIsPressedP = page.keyboard.press("V", { delay: 100 });
+                return AIsPressedP;
+            }).then(function () {
+                let ctrlIsPressedP = page.keyboard.up("Control");
+                return ctrlIsPressedP;
+            }).then(function () {
+                return page.click(".hr-monaco__run-code", { delay: 50 });
+            })
+            .then(function () {
+                resolve();
+            }).catch(function (err) {
+                console.log(err)
+                reject(err);
+            })
+    })
+}
+
+
+
+
+
+
