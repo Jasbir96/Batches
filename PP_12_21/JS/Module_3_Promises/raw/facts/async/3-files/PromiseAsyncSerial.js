1f4b51fa7d444a2cbf64cf544f241b5664f70e14
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-11 12:21:39 +0530

    added upto chaining

--- PP_12_21/JS/Module_3_Promises/raw/facts/async/3-files/PromiseAsyncSerial.js
+++ PP_12_21/JS/Module_3_Promises/raw/facts/async/3-files/PromiseAsyncSerial.js
@@ -1,20 +1,44 @@
 let fs = require("fs");
 console.log("Before");
+// brute force 
+// let ffReadPromise = fs.promises.readFile("f1.txt");
+// ffReadPromise
+//     .then(cb1);
+
+// function cb1(content) {
+//     console.log("content" + content);
+//     let SfReadPromise = fs.promises.readFile("f2.txt");
+//     SfReadPromise.then(cb2);
+// }
+// function cb2(content) {
+//     console.log("content" + content);
+//     let thfReadPromise = fs.promises.readFile("f3.txt");
+//     thfReadPromise.then(cb3);
+// }
+// function cb3(content) {
+//     console.log("content" + content);
+// }
+// console.log("After");
+// //  instead giving a cb function to an async function you attach
+// // the callback to the promise retruned by that function
+// function fcb(err) {
+//     console.log("err", err);
+// }
+
 let ffReadPromise = fs.promises.readFile("f1.txt");
-ffReadPromise
-    .then(cb1);
-ffReadPromise.catch(fcb);
+// initially there is a promise chain that is formed  those intrenally linked and later on there cbs are executed
+ffReadPromise.then(cb1).then(cb2).then(cb3);
 function cb1(content) {
     console.log("content" + content);
     let SfReadPromise = fs.promises.readFile("f2.txt");
-    SfReadPromise.then(cb2);
-    SfReadPromise.catch(fcb);
+    return SfReadPromise;
+
 }
 function cb2(content) {
     console.log("content" + content);
     let thfReadPromise = fs.promises.readFile("f3.txt");
-    thfReadPromise.then(cb3);
-    thfReadPromise.catch(fcb);
+    return thfReadPromise
+
 }
 function cb3(content) {
     console.log("content" + content);
