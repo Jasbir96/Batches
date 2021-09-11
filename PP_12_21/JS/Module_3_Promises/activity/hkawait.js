c080c3d8bef6d32572f95e3c04d8e07a3724d1dd
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-11 14:30:31 +0530

    added upto aa conversion

--- PP_12_21/JS/Module_3_Promises/activity/hkawait.js
+++ PP_12_21/JS/Module_3_Promises/activity/hkawait.js
@@ -13,54 +13,36 @@ let browserStartPromise = puppeteer.launch({
     args: ["--start-maximized", "--disable-notifications"]
 });
 let page, browser;
-browserStartPromise
-    .then(function (browserObj) {
+(async function fn() {
+    try {
+        let browserObj = await browserStartPromise;
         console.log("Browser opened");
-        // new tab 
         browser = browserObj
-        let browserTabOpenPromise = browserObj.newPage();
-        return browserTabOpenPromise;
-    }).then(function (newTab) {
-        page = newTab
-        console.log("new tab opened ")
-        let gPageOpenPromise = newTab.goto(loginLink);
-        return gPageOpenPromise;
-    }).then(function () {
+        // new tab 
+        page = await browserObj.newPage();
+        await newTab.goto(loginLink);
+        await page.type("input[id='input-1']", emailpassObj.email, { delay: 50 });
+        await page.type("input[type='password']", emailpassObj.password, { delay: 50 });
+        await page.click('button[data-analytics="LoginPassword"]', { delay: 100 });
+        await waitAndClick(".track-card a[data-attr2='algorithms']", page);
+        await waitAndClick("input[value='warmup']", page);
+        await page.waitFor(3000);
+        let questionsArr = await page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled"
+            , { delay: 100 });
+        for (let i = 0; i < questionsArr.length; i++) {
+            await questionSolver(page, questionsArr[i], answers[i]);
+        }
+    } catch (err) {
+        console.log(err);
+    }
 
-        let emailWillBeEnteredPromise = page.type("input[id='input-1']", emailpassObj.email, { delay: 50 });
-        return emailWillBeEnteredPromise;
-    }).then(function () {
-        let passwordWillBeEnteredPromise = page.type("input[type='password']", emailpassObj.password, { delay: 50 });
-        return passwordWillBeEnteredPromise;
-    }).then(function () {
-        let loginWillBeDOnepromise =
-            page.click('button[data-analytics="LoginPassword"]', { delay: 100 });
-        return loginWillBeDOnepromise;
-    })
-    .then(function () {
-        let clickedOnAlgoPromise =
-            waitAndClick(".track-card a[data-attr2='algorithms']", page);
-        return clickedOnAlgoPromise;
-    }).then(function () {
-        let getToWarmUp =
-            waitAndClick("input[value='warmup']", page);
-        return getToWarmUp;
-    }).then(function () {
-        let waitFor3SecondsPromise = page.waitFor(3000);
-        return waitFor3SecondsPromise;
-    }).then(function () {
-        let AllChallengesArrPromise =
-            page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled"
-                , { delay: 100 });
-        return AllChallengesArrPromise;
-    }).then(function (questionsArr) {
-        // n number of question first 
-        console.log("number of questions", questionsArr.length);
-        let qWillBeSolvedPromise = questionSolver(page, questionsArr[0], answers[0]);
-        return qWillBeSolvedPromise;
-    }).then(function () {
-        console.log("question is solved");
-    })
+})()
+
+
+
+
+// n number of question first 
+
 function waitAndClick(selector, cPage) {
     return new Promise(function (resolve, reject) {
         let waitForModalPromise = cPage.waitForSelector(selector, { visible: true });
