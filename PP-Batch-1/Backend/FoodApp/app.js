136b399d3e06becf6b8cabc1d27b4c74eb627f31
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-14 16:06:05 +0530

    added upto rest api examples

--- PP-Batch-1/Backend/FoodApp/app.js
+++ PP-Batch-1/Backend/FoodApp/app.js
@@ -1,7 +1,6 @@
 // npm init -y
 // npm i express
 // npm i nodemon -g 
-
 const express = require("express");
 // Server: // route  -> request -> response/file 
 // File system// path -> interact/type -> file /folder
@@ -11,46 +10,65 @@ const app = express();
 app.use(express.json());
 // function -> route  path
 // frontend -> req -> /
-app.get("/", function (req, res) {
-    console.log("hello from home page")
-    res.send("<h1>Hello from Backend</h1>");
-})
+
 let user = {
 };
 // getting data from server
 // giving data to server
 // crud app 
 // create
-app.post("/user", function (req, res) {
+// .form fill
+function createUser(req, res) {
     console.log("req.data", req.body);
     user = req.body;
     res.status(200).send("data recieved and user added ");
-})
-// get
-app.get("/user", function (req, res) {
+}
+function getUser(req, res) {
     console.log("users")
     res.json(user);
     // for sending key value pair
-})
-//update
-app.patch("/user", function (req, res) {
+}
+function updateUser(req, res) {
     let obj = req.body;
     for (let key in obj) {
         user[key] = obj[key];
     }
     res.status(200).json(user);
-})
-//delete
-app.delete("/user", function (req, res) {
+}
+function deletUser(req, res) {
     user = {}
     res.status(200).json(user);
-})
-
-//template routes 
-app.get("/user/:id", function (req, res) {
+}
+function getUserById(req, res) {
     console.log(req.params);
     res.status(200).send("Hello");
-})
+}
+
+// mounting in express 
+const router = express.Router();
+// /api/user/:id
+app.use('/api/user', router);
+
+router
+    .route("/")
+    .get(getUser)
+    .post(createUser)
+    .patch(updateUser)
+    .delete(deletUser);
+router
+    .route("/:id")
+    .get(getUserById);
+
+// app.post("/api/user", getUser);
+// // get
+// app.get("/api/user", createUser);
+// //update
+// app.patch("/api/user", updateUser);
+// //delete
+// app.delete("/api/user", deletUser);
+//template routes 
+// app.get("api/user/:id", getUserById);
+
 //localhost:8080 ??
 app.listen(8080, function () {
     console.log("server started");
