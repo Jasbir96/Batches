2ece82d3dc65b6f1d435c77aa1c308d3ced03b84
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-14 16:56:17 +0530

    added upto signup flow

--- PP-Batch-1/Backend/FoodApp/app.js
+++ PP-Batch-1/Backend/FoodApp/app.js
@@ -6,18 +6,49 @@ const express = require("express");
 // File system// path -> interact/type -> file /folder
 // server init
 const app = express();
-// post accept 
-app.use(express.json());
+// post accept -> folder designate  
+
 // function -> route  path
 // frontend -> req -> /
 
-let user = {
-};
 // getting data from server
 // giving data to server
 // crud app 
 // create
 // .form fill
+app.use(express.static('public'))
+app.use(express.json());
+const userRouter = express.Router();
+const authRouter = express.Router();
+// /api/user/:id
+app.use('/api/user', userRouter);
+app.use("/api/auth", authRouter);
+userRouter
+    .route("/")
+    .get(getUser)
+    .post(createUser)
+    .patch(updateUser)
+    .delete(deletUser);
+userRouter
+    .route("/:id")
+    .get(getUserById);
+authRouter
+    .post("/signup", signupUser)
+    .post("/login", loginUser);
+// database 
+let user = [];
+function signupUser(req, res) {
+    //email,user name ,password
+    let { email, password, name } = req.body;
+    console.log("user", req.body);
+    user.push({
+        email, name, password
+    })
+    res.status(200).json({
+        message: "user created",
+        createdUser: req.body
+    })
+}
 function createUser(req, res) {
     console.log("req.data", req.body);
     user = req.body;
@@ -44,20 +75,12 @@ function getUserById(req, res) {
     res.status(200).send("Hello");
 }
 
-// mounting in express 
-const router = express.Router();
-// /api/user/:id
-app.use('/api/user', router);
+function loginUser(req, res) {
 
-router
-    .route("/")
-    .get(getUser)
-    .post(createUser)
-    .patch(updateUser)
-    .delete(deletUser);
-router
-    .route("/:id")
-    .get(getUserById);
+}
+
+// mounting in express 
+
 
 // app.post("/api/user", getUser);
 // // get
