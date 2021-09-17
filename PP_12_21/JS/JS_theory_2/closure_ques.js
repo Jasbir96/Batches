0c9e529356dc12056d04c9c04b4642f9e5919af4
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-17 20:53:18 +0530

    added upto closure examples

--- PP_12_21/JS/JS_theory_2/closure_ques.js
+++ PP_12_21/JS/JS_theory_2/closure_ques.js
@@ -42,18 +42,48 @@
 // arrFno[0]();
 // arrFno[1]();
 // arrFno[2]();
-// function outer() {
-//     let arrFn = [];
-//     for (let i = 0; i < 3; i++) {
-//         arrFn.push(function fn() {
-//             console.log(i);
-//         })
-//     }
-//     return arrFn;
+
+function outer() {
+    let arrFn = [];
+    // local -> 3
+    // let i = 0
+    // 
+    // block 0,block 1 ,block 2
+    for (let i=0; i < 3; i++) {
+        // block scope
+        arrFn.push(function fn() {
+            console.log(i);
+        })
+    }
+    return arrFn;
+}
+let arrFn = outer();
+
+arrFn[0]();
+arrFn[1]();
+arrFn[2]();
+
+function createIncrement() {
+    let count = 0;
+    function increment() {
+        count++;
+    }
+    let message = `Count is ${count}`;
+    function log() {
+        console.log(count);
+        console.log(message);
+    }
+    return [increment, log];
+}
+const arr = createIncrement();
+let increment = arr[0];
+let log = arr[1];
+increment();
+increment();
+increment();
+log();
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(function () {
+//         console.log(i)
+//     }, 1000);
 // }
-// let arrFn = outer();
-
-// arrFn[0]();
-// arrFn[1]();
-// arrFn[2]();
-
