d3751c2db2f3028ba91f8502fd7b816c808c9736
Jasbir <mr.jasbirsingh96@gmail.com>
2021-09-30 23:30:51 +0530

    added upto signup

--- PAB/Backend/rev/app1.js
+++ PAB/Backend/rev/app1.js
@@ -1,8 +1,8 @@
 // npm init -y
 // npm i express
-const { next } = require("cheerio/lib/api/traversing");
 const express = require("express");
-let fs = require("fs");
+const fs = require("fs");
+const path = require("path");
 // Server: // route  -> request -> response/file 
 // File system// path -> interact/type -> file /folder
 // server init
@@ -16,38 +16,68 @@ const app = express();
 // })
 // inbuilt menthods of express has next already implmeneted
 // always use me
-//  express json -> req.body add  
+//  express json -> req.body add
+// reserve a folder only from which client can acces the files 
+app.use(express.static("Frontend_folder"));
 app.use(express.json());
 
 // // function -> route  path
 // // frontend -> req -> /
 // read data storage
 // localhost/user/10 -> post 
-// let content = JSON.parse(fs.readFileSync("./data.json"));
+let content = JSON.parse(fs.readFileSync("./data.json"));
 // const userRouter = express.Router();
-// const authRouter = express.Router();
+const authRouter = express.Router();
 // // localhost / auth / 10-> patch
 // app.use('/user', userRouter);
-// app.use('/auth', authRouter);
+app.use('/auth', authRouter);
 // userRouter
-    // .route('/')
-    // localhost/user -> get
-    // .get(getUsers)
-    // localhost/user -> post
-    // .post(bodyChecker, isAuthenticated, isAuthorized, createUser);
-
-
+//     .route('/')
+//     localhost/user -> get
+//     .get(getUsers)
+//     localhost/user -> post
+//     .post(bodyChecker, isAuthenticated, isAuthorized, createUser);
 // userRouter
 //     .route("/:id")
 //     // localhost/user/10-> post
 //     .get(getUser)
 
-// authRouter.route("/").post(signupUser)
-
+authRouter.route("/signup")
+    .post(bodyChecker, signupUser);
+function bodyChecker(req, res, next) {
+    console.log("reached body checker");
+    let isPresent = Object.keys(req.body).length;
+    console.log("ispresent", isPresent)
+    if (isPresent) {
+        next();
+    } else {
+        res.send("kind send details in body ");
+    }
+}
+function signupUser(req, res) {
+    let { name, email, password,
+        confirmPassword } = req.body;
+        console.log("req.body", req.body)
+    if (password == confirmPassword) {
+        let newUser = { name, email, password }
+        // entry put 
+        content.push(newUser);
+        // save in the datastorage
+        fs.writeFileSync("data.json",
+            JSON.stringify(content));
+        res.status(201).json({
+            createdUser: newUser
+        })
+    } else {
+        res.status(422).json({
+            message: 
+            "password and confirm password do not match"
+        })
+    }
+}
 // authRouter.route("/:id").patch(forgetPassword)
 // function createUser(req, res) {
 //     console.log("create users");
-
 //     let body = req.body;
 //     console.log("req.body", req.body);
 //     content.push(body);
@@ -55,33 +85,19 @@ app.use(express.json());
 //     fs.writeFileSync("./data.json", JSON.stringify(content));
 //     res.json({ message: content });
 // }
-// function bodyChecker(req, res, next) {
-//     console.log("reached body checker");
-//     let isPresent = Object.keys(req.body).length;
-
-//     console.log("ispresent", isPresent)
-//     if (isPresent) {
-//         next();
-//     } else {
-//         res.send("kind send details in body ");
-//     }
-// }
 // function getUsers(req, res) {
 //     res.json({ message: content });
 // }
-
 app.listen(8081, function () {
     console.log("server started");
 })
-
-
 app.post("/", function (req, res, next) {
     let body = req.body;
     console.log("inside first post", body);
     next();
 })
 app.use(function (req, res, next) {
-   console.log("inside app.use",)
+    console.log("inside app.use",)
     next();
 })
 app.get("/", function (req, res) {
@@ -93,6 +109,11 @@ app.post("/", function (req, res, next) {
     let body = req.body;
     console.log("inside second post ", body);
     res.send("tested next");
+})
+app.use(function (req, res) {
+    // console.log("fullPath", fullPath);
+    res.status(404).sendFile
+    (path.join(__dirname, "404.html"));
 })
 // app.get("/", function (req, res) {
 //     console.log("hello from home page")
