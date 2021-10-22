ca8c6cfec08a80d46fca9ccea5d831e9812aa64e
Jasbir <mr.jasbirsingh96@gmail.com>
2021-10-22 21:07:05 +0530

    added upto left col

--- PP_12_21/Browser/Module_4_Excel_Clone/script.js
+++ PP_12_21/Browser/Module_4_Excel_Clone/script.js
@@ -22,11 +22,11 @@ for (let i = 0; i < 26; i++) {
     div.textContent = String.fromCharCode(65 + i);
     topRow.appendChild(div)
 }
-// let leftCol = document.querySelector(".left_col");
-// for (let i = 1; i <= 100; i++) {
-//     let div = document.createElement("div");
-//     div.setAttribute("class", "cell");
-//     div.textContent = i;
-//     leftCol.appendChild(div)
-// }
+let leftCol = document.querySelector(".left_col");
+for (let i = 1; i <= 100; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "cell");
+    div.textContent = i;
+    leftCol.appendChild(div)
+}
 // 2 d loop -> columns*rows
