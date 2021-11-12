a81d8c61c066f1160c79500c11be9c75c1d8ea27
Jasbir <mr.jasbirsingh96@gmail.com>
2021-11-12 22:44:01 +0530

    added upto coor change in ui

--- PP_12_21/Browser/Module_4_Excel_Clone/menu.js
+++ PP_12_21/Browser/Module_4_Excel_Clone/menu.js
@@ -4,6 +4,38 @@ let boldIcon = document.querySelector(".fa-bold");
 let underlineIcon = document.querySelector(".fa-underline");
 let italicIcon = document.querySelector(".fa-italic");
 let alignmentContainer = document.querySelector(".alignment_container");
+let textColorHInput = document.querySelector(".text_color");
+let textColorInput = document.querySelector(".fa-tint");
+let backgroundHInput = document.querySelector(".background_color");
+let backgroundInput = document.querySelector(".fa-fill-drip");
+textColorInput.addEventListener("click", function (e) {
+    //dom help hidden click trigger 
+    textColorHInput.click();
+    // console.log("clicked");
+})
+textColorHInput.addEventListener("change", function (e) {
+    let color = textColorHInput.value;
+    // console.log(color);
+    let address = addressInput.value;;
+    let ridcidObj = getRidCidFromAddress(address);
+    let tobeChangedCell = document.querySelector
+        (`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`);
+    tobeChangedCell.style.color = color;
+})
+backgroundInput.addEventListener("click", function (e) {
+    //   dom help hidden click trigger 
+    backgroundHInput.click();
+    console.log("clicked");
+})
+backgroundHInput.addEventListener("change", function (e) {
+    let color = backgroundHInput.value;
+    // console.log(color);
+    let address = addressInput.value;;
+    let ridcidObj = getRidCidFromAddress(address);
+    let tobeChangedCell = document.querySelector
+        (`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`);
+    tobeChangedCell.style.backgroundColor = color;
+})
 // change
 fontSizeInput.addEventListener("change", function () {
     let fontSize = fontSizeInput.value;
@@ -24,7 +56,6 @@ fontFamilyInput.addEventListener("change", function () {
     // change fontSize property
     tobeChangedCell.style.fontFamily = fontFamily;
 })
-
 boldIcon.addEventListener("click", function () {
     // Ui se address get 
     let address = addressInput.value;
@@ -36,7 +67,6 @@ boldIcon.addEventListener("click", function () {
     // icon change kar do 
     boldIcon.classList.add("selected");
 })
-
 underlineIcon.addEventListener("click", function () {
     // Ui se address get 
     let address = addressInput.value;
@@ -48,7 +78,6 @@ underlineIcon.addEventListener("click", function () {
     // icon change kar do 
     underlineIcon.classList.add("selected");
 })
-
 italicIcon.addEventListener("click", function () {
     // Ui se address get 
     let address = addressInput.value;
@@ -71,5 +100,9 @@ alignmentContainer.addEventListener("click", function (e) {
             (`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`);
         // change fontSize property
         tobeChangedCell.style.textAlign = hAlignment;
+        // *********Bug prone
+        e.target.classList.add("selected");
     }
 })
+
+// colors -> 
