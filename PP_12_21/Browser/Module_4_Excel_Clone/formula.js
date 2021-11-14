8b78c090e5cc681f521980498190366d1a0bbd2d
Jasbir <mr.jasbirsingh96@gmail.com>
2021-11-14 23:42:43 +0530

    added upto formula implementation

--- PP_12_21/Browser/Module_4_Excel_Clone/formula.js
+++ PP_12_21/Browser/Module_4_Excel_Clone/formula.js
@@ -1,28 +1,38 @@
-
+// cell -> formula remove /value set 
 for (let i = 0; i < AllGridCells.length; i++) {
     AllGridCells[i].addEventListener("blur", function () {
         let content = AllGridCells[i].textContent;
         let address = addressInput.value;
         let { rid, cid } = getRidCidFromAddress(address);
-        // db[ridcidObj.rid][ridcidObj.cid].value = content;
+        let cellObject = db[rid][cid];
         // set self ui as well as children new values and ui
+        if (cellObject.formula) {
+            removeFormula(address, formula);
+            cellObject.formula = "";
+        }
         setUI(content, rid, cid);
     })
 }
 // formula bar
+// set Formula/update formula
 formulaInput.addEventListener("keydown", function (e) {
     if (e.key == "Enter" && formulaInput.value != "") {
         // fomrula get
         let cFormula = formulaInput.value;
+
         // address -> jispe formula jo value get wo set ho jaayegi 
         let addressOfTheCell = addressInput.value;
         let { rid, cid } = getRidCidFromAddress(addressOfTheCell);
+        let cellObject = db[rid][cid];
+        if (cellObject.formula != cFormula) {
+            removeFormula(addressOfTheCell, formula);
+        }
         // console.log(cFormula);
         let value = evaluateFormula(cFormula);
         console.log(value);
         setUI(value, rid, cid);
-        db[rid][cid].formula = cFormula;
-        setParent(addressOfTheCell, cFormula);
+        cellObject.formula = cFormula;
+        setFormula(addressOfTheCell, cFormula);
 
     }
 })
@@ -45,7 +55,7 @@ function evaluateFormula(formula) {
     }
     console.log(formula);
     // eval -> evaluate-> inbuilt 
-    let result = eval(formula); 
+    let result = eval(formula);
     return result;
 }
 function setUI(value, rid, cid) {
@@ -58,13 +68,13 @@ function setUI(value, rid, cid) {
     for (let i = 0; i < childrenArr.length; i++) {
         let chriciobj = getRidCidFromAddress(childrenArr[i]);
         let chCellObj = db[chriciobj.rid][chriciobj.cid];
-        let value=  evaluateFormula(chCellObj.formula);
-        setUI(value,chriciobj.rid,chriciobj.cid)
+        let value = evaluateFormula(chCellObj.formula);
+        setUI(value, chriciobj.rid, chriciobj.cid)
     }
 
 }
 //  to set a cell as children of a cell jispe depenedent 
-function setParent(address, formula) {
+function setFormula(address, formula) {
     console.log(formula);
     // ( A1 + A2 ) -> ( 10 + 20 )
     let formulaEntities = formula.split(" ");
@@ -78,6 +88,28 @@ function setParent(address, formula) {
             // db -> value
             let children = db[parentrcObj.rid][parentrcObj.cid].children;
             children.push(address);
+            // replace in formula
+
+        }
+    }
+    console.log(formula);
+}
+//  to set a cell as children of a cell jispe depenedent 
+function removeFormula(address, formula) {
+    console.log(formula);
+    // ( A1 + A2 ) -> ( 10 + 20 )
+    let formulaEntities = formula.split(" ");
+    // [(,A1,+,A2,)]
+    console.log(formulaEntities);
+    for (let i = 0; i < formulaEntities.length; i++) {
+        let ascii = formulaEntities[i].charCodeAt(0);
+        if (ascii >= 65 && ascii <= 90) {
+            // address -> rid cId
+            let parentrcObj = getRidCidFromAddress(formulaEntities[i]);
+            // db -> value
+            let children = db[parentrcObj.rid][parentrcObj.cid].children;
+            let idx = children.indexOf(address);
+            children.slice(idx, 1);
             // replace in formula
 
         }
