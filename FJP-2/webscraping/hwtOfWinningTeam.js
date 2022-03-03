dbacaa66a6abc27c4412e2d040f9d8e3cad4a801
Jasbir <mr.jasbirsingh96@gmail.com>
2022-03-03 23:37:23 +0530

    added after team name

---
+++ FJP-2/webscraping/hwtOfWinningTeam.js
@@ -0,0 +1,54 @@
+// npm i request
+let request = require('request');
+// npm i jsdom 
+// data extract 
+let jsdom = require("jsdom");
+let fs = require("fs");
+let { JSDOM } = jsdom;
+let url = "https://www.espncricinfo.com/series/afghanistan-in-bangladesh-2021-22-1299826/bangladesh-vs-afghanistan-1st-t20i-1299832/full-scorecard";
+// callback -> request -> data get
+request(url, cb);
+// cb is called by your request 
+// html -> data 
+function cb(err, httpResponse, html) {
+    if (err) {
+        console.log(err);
+    } else if (httpResponse.statusCode == 404) {
+        console.log("Page not found");
+    } else {
+        // console.log(html);
+        console.log("Html data recieved");
+        parseHtml(html);
+    }
+}
+function parseHtml(html) {
+    let dom = new JSDOM(html);
+    let MyDocument = dom.window.document;
+    // first result 
+    // queryselector
+    // multiple array -> querysSelectorAll
+    // , textContext, getAttribute
+    let teamName = MyDocument.querySelector
+            (".match-info.match-info-MATCH.match-info-MATCH-half-width .status-text");
+    // tag -> property get 
+    let title = teamName.textContent;
+    let teamNameArr = title.split("w");
+    let WinningTeamName = teamNameArr[0].trim();
+    // all collapsibles  -> team -> team details 
+    let bothInningHtml = MyDocument.querySelectorAll(".Collapsible");
+    for (let i = 0; i < bothInningHtml.length; i++) {
+        let singleInning = bothInningHtml[i];
+        // team name get 
+        let teamNameHtml = singleInning.querySelector("h5");
+        //    fs.writeFileSync(`inning${i+1}.html`,inningHtml);
+        // name correct -> name compare -> 
+        let teamNameRaw = teamNameHtml.textContent;
+        let teamNameArr = teamNameRaw.split("INNINGS");
+        let teamName = teamNameArr[0].trim();
+        if (WinningTeamName == teamName) {
+            // singleInnings[i] -> bowlers task complete 
+            console.log("Winnning team is " + finalTeamName);
+            // code -> single inning 
+        }
+    }
+}
---
+++ FJP-2/webscraping/mom.js
@@ -0,0 +1,35 @@
+// npm i request
+let request = require('request');
+// data extract 
+let jsdom = require("jsdom");
+let { JSDOM } = jsdom;
+let url = "https://www.espncricinfo.com/series/afghanistan-in-bangladesh-2021-22-1299826/bangladesh-vs-afghanistan-1st-t20i-1299832/full-scorecard";
+// callback -> request -> data get
+request(url, cb);
+// cb is called by your request 
+// html -> data 
+function cb(err, httpResponse, html) {
+    if (err) {
+        console.log(err);
+    } else if (httpResponse.statusCode == 404) {
+        console.log("Page not found");
+    } else {
+        // console.log(html);
+        console.log("Html data recieved");
+        parseHtml(html);
+    }
+}
+function parseHtml(html) {
+    let dom = new JSDOM(html);
+    let MyDocument = dom.window.document;
+    // first result 
+    // queryselector
+    // multiple array -> querysSelectorAll
+    // , textContext, getAttribute
+    let mom =
+    MyDocument
+    .querySelector(".playerofthematch-name");
+    // tag -> property get 
+    let title = mom.textContent;
+    console.log(title);
+}
