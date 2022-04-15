5b3c11aaed748ab3e63579ac3ecd9abd9dcdd6bc
Jasbir <mr.jasbirsingh96@gmail.com>
2022-04-15 23:12:04 +0530

    added create and insert in sql

---
+++ Web-march-22/Module_1_SQL/create_insert_select.sql
@@ -0,0 +1,18 @@
+-- create table query
+CREATE TABLE emp(
+    id INTEGER AUTO_INCREMENT UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    phno BIGINT UNIQUE,
+    salary FLOAT NOT NULL CHECK (salary>2500),
+    dep VARCHAR(255),
+    age INTEGER DEFAULT 25
+    gender ENUM("M","F","TG")
+);
+
+
+-- INSERT  certain column values
+INSERT INTO emp (name,salary,gender)
+VALUES("JASBIR",5000,"M");
+
+-- you need to put all the entries
+INSERT INTO emp VALUES(3,"Anuj",123456789,5000,"Tech",25,"M");
