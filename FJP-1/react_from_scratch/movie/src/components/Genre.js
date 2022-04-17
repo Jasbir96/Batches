cc25735963b45bc7e1e03d6ee3296a28a9451341
Jasbir <mr.jasbirsingh96@gmail.com>
2022-04-17 19:56:20 +0530

    added genre.js

--- FJP-1/react_from_scratch/movie/src/components/Genre.js
+++ FJP-1/react_from_scratch/movie/src/components/Genre.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Genre() {
+  const [isLoaded, setLoaded] = useState(true);
+  const [content, setContent] = useState([]);
+  // so i will run only one time after first execution of return statement  
+  useEffect(async function () {
+    // fetch is inbuilt feature of browser that makes the request to get data -> promise based
+    let response = await fetch('https://react-backend101.herokuapp.com/genres');
+    response = await response.json();
+    // console.log(response); 
+    setLoaded(false);
+    setContent(response);
+  }, [])
+
   return (
-    <div className="Genre">
+  <div className="Genre">
       <div className="
       mr-6 border-2 w-40 text-center h-10 font-bold">All Genre</div>
-      <div className="
-      mr-6 border-2 w-40 text-center h-10 border-t-0 font-bold">Action</div>
-      <div className="
-      mr-6 border-2 w-40 text-center h-10 border-t-0 font-bold">Comedy</div>
-      <div className="
-      mr-6 border-2 w-40 text-center h-10 border-t-0 font-bold">Thriller</div>
-    </div>
+      {isLoaded == true ?
+      <div className="font-bold"> Loading...</div >
+        : content.genres.map(function (genre) {
+          return( < div className="mr-6 border-2 w-40 text-center h-10 border-t-0 font-bold">{genre.name}</div>
+            )}
+          )
+      }
+    </div >
+
+
   )
 }
 
