54a9d05061702fd30376bcf8acfd70b4dd2be5c0
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-07 23:49:51 +0530

    added upto useEffect

---
+++ FJP-1/react_from_scratch/reels/src/POCS/useState/usereducerComplex.js
@@ -0,0 +1,35 @@
+import React, { useReducer } from 'react'
+function UseReducer() {
+    // 1. initial state variable
+    const intialState = {
+        value: 0
+    };
+    // jo bhi aapke function they jo 
+    // state change karenge unka logic kisme switch case ke andar
+    // un state ko change karne combined function pass
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "increment":
+                return { ...state, value: action.value }
+            case "decrement":
+                return { ...state, value: action.value };
+            case "IncrementByFive":
+                return { ...state, value: state.value + 5 };
+
+            default:
+                return state;
+        }
+    }
+    // dispatch-> reducer-> state change
+    let [countObj, dispatch] = useReducer(intialState, reducer);
+
+    return (
+        <>
+            <button onClick={dispatch({ type: "increment", value: 10 })}>Increment</button>
+            <span>    {countObj.value}    </span>
+            <button onClick={dispatch({ type: "decrement", value: 5 })}>Decrement</button>
+            <button onClick={dispatch({ type: "IncrementByFive" })}> five </button>
+        </>
+    )
+}
+export default UseState;
