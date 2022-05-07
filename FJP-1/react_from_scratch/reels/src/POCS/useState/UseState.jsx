b6c0266a955f223f27a96df66274e321597b1a78
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-07 22:48:52 +0530

    added upto context and ReactmemoExample

---
+++ FJP-1/react_from_scratch/reels/src/POCS/useState/UseState.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UseState() {
+    return (
+        <div>UseState</div>
+    )
+}
+
+export default UseState
--- FJP-1/react_from_scratch/reels/src/index.js
+++ FJP-1/react_from_scratch/reels/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import UseffectDEMO from './UseffectDEMO';
-
 // import Context from './contextPOCs/theme/Context';
 // import ThemeManager from "./contextPOCs/ThemeChanger";
 import { BrowserRouter } from "react-router-dom";
@@ -13,8 +12,10 @@ root.render(
     // <BrowserRouter>
     //     <App />
     // </BrowserRouter>
+// useState
 
-    <ContextNormal></ContextNormal>
+
+    // <ContextNormal></ContextNormal>    
     // <UseffectDEMO></UseffectDEMO>
     // <Context />
     // <ThemeManager></ThemeManager>
