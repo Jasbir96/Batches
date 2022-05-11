899312467c1f49190547ca827bcfd0c92ce2ed53
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-11 21:51:52 +0530

    added upto input in file system

--- Web-march-22/Module_1/L4_File_organizer/FilSystemOrganizer/cli.js
+++ Web-march-22/Module_1/L4_File_organizer/FilSystemOrganizer/cli.js
@@ -3,3 +3,20 @@
 // nodejs cli.js help -> print Help command executed
 // nodejs cli.js tree -> print tree command executed
 // nodejs cli.js organize -> print tree command executed
+
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+let dirPath = inputArr[1];
+switch (command) {
+    case "help":
+        console.log("Help Command executed");
+        break;
+    case "tree":
+        console.log("Tree command Executed with the path", dirPath != undefined ? dirPath : process.cwd());
+        break;
+    case "organize":
+        console.log("Organize command executed", dirPath != undefined ? dirPath : process.cwd());
+        break;
+    default:
+        console.log("Wrong command . Type help to see the list of all the commands");
+}
