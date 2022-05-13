d21eeff1e5cd9a6fa287b66927caa97b99e5ba5d
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-13 22:35:25 +0530

    added upto organize

--- Web-march-22/Module_1/L4_File_organizer/FilSystemOrganizer/commands/organize.js
+++ Web-march-22/Module_1/L4_File_organizer/FilSystemOrganizer/commands/organize.js
@@ -2,6 +2,13 @@ function organizeFn(dirPath) {
     console.log("Organize command Executed with the path",
         dirPath != undefined ? dirPath : process.cwd());
 }
+let types = {
+    media: ["mp4", "mkv", "mp3"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+
 module.exports = {
     organizeFn: organizeFn
 }
