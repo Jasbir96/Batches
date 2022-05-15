25ecd0ecb9ff540a4186705c95b4379ea61b4e4a
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-15 22:44:37 +0530

    added upto all the components

--- FJP-1/react_from_scratch/reelsnew/src/components/Login.jsx
+++ FJP-1/react_from_scratch/reelsnew/src/components/Login.jsx
@@ -4,6 +4,10 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
+  
+  let [user, setUser] = useState(null);
+  let [loader, setLoader] = useState(false);
+  let [error, setError] = useState("");
   const trackEmail = function (e) {
     setEmail(e.target.value);
   }
@@ -11,22 +15,38 @@ function Login() {
     setPassword(e.target.value);
   }
   const printDetails = async function () {
-    alert(email + " " + password);
-    let userCred = await 
-    signInWithEmailAndPassword(auth, email, password)
-    console.log(userCred.user);
-    
-
+    // alert(email + " " + password);
+    try {
+      setLoader(true);
+      let userCred = await
+        signInWithEmailAndPassword(auth, email, password)
+      // console.log(userCred.user);
+      setUser(userCred.user);
+    } catch (err) {
+      setError(err.message);
+      // after some time -> error message remove 
+      setTimeout(() => {
+        setError("")
+      }, 2000)
+    }
+    setLoader(false);
   }
   return (
     <>
-      <input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
-      <br></br>
-      <input type="password" onChange={trackPassword} value={password} placeholder="password"
-      ></input>
-      <br></br>
-      <button type="click" onClick={printDetails}
-      >Login</button>
+      {
+        error != "" ? <h1>Error is {error}</h1> :
+          loader == true ? <h1>...Loading</h1> :
+            user != null ? <h1>user is {user.uid}</h1>
+              :
+              <><input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
+                <br></br>
+                <input type="password" onChange={trackPassword} value={password} placeholder="password"
+                ></input>
+                <br></br>
+                <button type="click" onClick={printDetails}
+                >Login</button></>
+      }
+
     </>
   )
 }
