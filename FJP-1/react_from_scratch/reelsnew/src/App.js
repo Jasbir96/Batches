691b75a2245d3c86ade4b23757835dea6f6e6738
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-15 19:19:31 +0530

    added till upto routing

--- FJP-1/react_from_scratch/reelsnew/src/App.js
+++ FJP-1/react_from_scratch/reelsnew/src/App.js
@@ -1,8 +1,36 @@
+// npm i react-router-dom@5.3.1
 import React from 'react';
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div>Reels Project</div>
+    <>
+
+      {/* which component should render on which path */}
+      <Switch>
+        <Route path="/feed">
+          <Feed></Feed>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+
+        </Route>
+        <Route path="/signup">
+          <Signup></Signup>
+        </Route>
+        <Route path="/profile">
+          <Profile></Profile>
+        </Route>
+        <Route>
+          <PageNotFound></PageNotFound>
+        </Route>
+      </Switch>
+
+    </>
   );
 }
-
 export default App;
