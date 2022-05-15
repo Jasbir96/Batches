7185b2c0309a239bb03389b3a9fdce9b5336e775
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-15 19:44:35 +0530

    added upto sample login

--- FJP-1/react_from_scratch/reelsnew/src/components/Login.jsx
+++ FJP-1/react_from_scratch/reelsnew/src/components/Login.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  const trackEmail = function (e) {
+    setEmail(e.target.value);
+  }
+  const trackPassword = function (e) {
+    setPassword(e.target.value);
+  }
+  const printDetails = function () {
+    alert(email + " " + password);
+  }
   return (
-    <div>Login</div>
+    <>
+      <input type="email" onChange={trackEmail}  value={email} placeholder="email" ></input>
+      <br></br>
+      <input type="password" onChange={trackPassword} value={password}  placeholder="password"
+      ></input>
+      <br></br>
+      <button type="click" onClick={ printDetails}
+      >Login</button>
+    </>
   )
 }
 
