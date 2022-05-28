d195af81d7a53da90dc19722b1cfc8cea1ac1eef
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-28 22:05:14 +0530

    added upto protectRoute

--- FJP-1/react_from_scratch/reelsnew/src/App.js
+++ FJP-1/react_from_scratch/reelsnew/src/App.js
@@ -1,36 +1,68 @@
 // npm i react-router-dom@5.3.1
-import React from 'react';
+import React, { useContext } from 'react';
 import Feed from "./components/Feed";
 import Login from "./components/Login";
 import PageNotFound from "./components/PageNotFound";
 import Profile from "./components/Profile";
 import Signup from "./components/Signup";
-import {AuthContextProvider} from "./context/AuthContext";
-import { Switch, Route } from "react-router-dom";
+import { AuthContextProvider, AuthContext } from "./context/AuthContext";
+import { Switch, Route, Redirect } from "react-router-dom";
 function App() {
   return (
     <>
       {/* which component should render on which path */}
       <AuthContextProvider>
         <Switch>
-          <Route path="/feed">
-            <Feed></Feed>
-          </Route>
           <Route path="/login">
             <Login></Login>
           </Route>
           <Route path="/signup">
             <Signup></Signup>
           </Route>
-          <Route path="/profile">
-            <Profile></Profile>
-          </Route>
+          <PrivateRoute path="/feed"
+            comp={<Feed></Feed>}
+          >
+          </PrivateRoute>
+          <PrivateRoute path="/profile"
+            comp={<Profile></Profile>}
+          >
+          </PrivateRoute>
           <Route>
             <PageNotFound></PageNotFound>
           </Route>
+
+          {/* <Route path="/">
+            <Feed></Feed>
+          </Route>
+          <Route path="/" render={(props) => {
+            return <Feed {...props}></Feed>
+          }}></Route> */}
         </Switch>
       </AuthContextProvider>
     </>
   );
 }
+function PrivateRoute(props) {
+  console.log("private Route",props)
+  let Component = props.comp;
+  // check -> are you loggedIN
+  let cUser = useContext(AuthContext);
+  // cuser-> null ->logi page
+  // cuser-> anything 
+  return (
+    <Route
+      {...props}
+      render={
+        (props) => {
+          // logic
+          return cUser != null ? <Component {...props}>
+          </Component> : <Redirect 
+          {...props}
+          to="/login"></Redirect>
+        }
+      }
+    ></Route>
+  )
+}
+
 export default App;
--- FJP-1/react_from_scratch/reelsnew/src/context/AuthContext.js
+++ FJP-1/react_from_scratch/reelsnew/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 export const AuthContext = React.createContext();
-
+// children is an inbuilt prop -> it refers to all the component specified inside 
 export function AuthContextProvider({ children }) {
     let [mainLoader, setMainLoader] = useState(true);
     let [cUser, setUser] = useState(null);
@@ -11,7 +11,7 @@ export function AuthContextProvider({ children }) {
             if (user) {
                 // User is signed in, see docs for a list of available properties
                 // https://firebase.google.com/docs/reference/js/firebase.User
-                setUser(user)
+                setUser(user);
                 // ...
             } else {
                 // User is signed out
