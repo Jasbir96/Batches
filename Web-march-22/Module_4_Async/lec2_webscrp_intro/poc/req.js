5d944afdf6635d61bd2b30777852fc21da32b3d7
Jasbir <mr.jasbirsingh96@gmail.com>
2022-05-30 22:25:26 +0530

    added till html file creation

--- Web-march-22/Module_4_Async/lec2_webscrp_intro/poc/req.js
+++ Web-march-22/Module_4_Async/lec2_webscrp_intro/poc/req.js
@@ -1,20 +1,25 @@
 
 // 1. npm i request -> npm 
 const request = require('request');
+const fs = require("fs");
 let url = 'http://www.google.com';
-
 // request working-> 
 // 1. request to the given url;
 // 2.  response -> request function -> cb call -> data put 
-
 request(url, cb);
-
+// params
 // error ->if there is any error while executing the request 
 // response -> header + body 
 // body -> html
-
+console.log('Before')
 function cb(error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    if (error) {
+        console.log('error:', error.message); // Print the error message
+    } else if (response && response.statusCode == 404) {
+        console.log("Page not found");
+    } else {
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        fs.writeFileSync("index.html", body);
+    }
 }
+console.log('After');
