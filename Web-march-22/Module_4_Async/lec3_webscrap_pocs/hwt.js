4b7beed9eba75aeff5ee70fb584fcb2c7c036d10
Jasbir <mr.jasbirsingh96@gmail.com>
2022-06-03 22:48:20 +0530

    added upto players

--- Web-march-22/Module_4_Async/lec3_webscrap_pocs/hwt.js
+++ Web-march-22/Module_4_Async/lec3_webscrap_pocs/hwt.js
@@ -24,16 +24,36 @@ function extractData(body) {
     // 2. // no meaning 
     // document represent the whole html page 
     let document = dom.window.document;
-    let output = document.querySelectorAll(".ds-text-compact-xxs.ds-p-2.ds-px-4 p>span");
-    let resultElem = output[0];
-    // textcontent 
-    let res = resultElem.textContent;
-    console.log("result :", res);
+    //  4 matches -> batting,bowling 
+    let allTables = document.querySelectorAll
+        ("table.ds-w-full.ds-table.ds-table-xs.ds-table-fixed");
+
+    let firstInningBowling = allTables[1]; // bowling table 1
+    let secondInningBowling = allTables[3]; //bowlling table 2
+    // element  -> ke anadr jo bhi html wo inne html se  aa jayegi 
+    let newHtmlString = "<table>" + firstInningBowling.innerHTML + "</table>"
+        + "<table>" + secondInningBowling.innerHTML + "</table>";
+    // fs.writeFileSync("tables.html", newHtmlString);
+    // console.log(newHtmlString);
+    getDataFromBowlingTables(newHtmlString);
+    // how to narrow;
     // using document and your selectors you find element in html page 
     console.log("reached for parsing");
 }
 
+function getDataFromBowlingTables(newHtmlString) {
+    const JSDOM = jsdom.JSDOM;
+    // pass to newJSDOM 
+    let dom = new JSDOM(newHtmlString);
+    // 2. // no meaning 
+    // document represent the whole html page 
+    let document = dom.window.document;
 
-{/* <p class="ds-text-tight-m ds-font-regular ds-truncate ds-text-typo-title"></p>  */}
+    let allRows = document.querySelectorAll("tbody tr.ds-text-tight-s");
+console.log(allRows.length);
+
+}
+
+
 
 
