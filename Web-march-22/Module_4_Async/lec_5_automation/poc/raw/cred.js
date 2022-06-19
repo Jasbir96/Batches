0cebb4e90fe7c5529b1cbe1fd0d9e38ab26a3bb7
Jasbir <mr.jasbirsingh96@gmail.com>
2022-06-19 12:39:04 +0530

    added upto login logic

---
+++ Web-march-22/Module_4_Async/lec_5_automation/poc/raw/cred.js
@@ -0,0 +1,4 @@
+module.exports = {
+    email: "texixis959@tagbert.com",
+    password: "123456789"
+}
--- Web-march-22/Module_4_Async/lec_5_automation/poc/raw/login.js
+++ Web-march-22/Module_4_Async/lec_5_automation/poc/raw/login.js
@@ -1,31 +1,34 @@
+
 // browser control
 // controls a headless browser -> browser that is by default not visible 
 // npm i puppeteer
 const puppeteer = require("puppeteer");
 // nearly every function of puppeteer returns a promise
+const credObj = require("./cred");
+// module.exports = {
+//     password: "",
+//     email: ""
+// }
 async function fn() {
     const browserRepresentativeObj = await puppeteer.launch({
         headless: false,
         executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
         defaultViewport: null,
-        args: ["--start-maximized","--start-in-incognito"],
-        slowMo:100
+        args: ["--start-maximized", "--start-in-incognito"],
+        slowMo: 100
     });
     //new  tab open  
     const tab = await browserRepresentativeObj.newPage();
     // // to go google's home page 
-    await tab.goto("https://www.google.com");
-    // // text type 
-    await tab.type("input[type='text']", "pepcoding", { delay: 200 });
-    // // /to press a specific key
+    await tab.goto("https://www.hackerrank.com/auth/login");
+    await tab.type("input[type='text']", credObj.email, { delay: 20 });
+    await tab.type("input[type='password']", credObj.password, { delay: 20 });
     await tab.keyboard.press("Enter");
-    // // // page change error prevent  -> to wait for selector that is present on the second page  
-    await tab.waitForSelector(".LC20lb.MBeuO.DKV0Md", { visible: true });
-    // // click 
-    await tab.click(".LC20lb.MBeuO.DKV0Md", { delay: 200 });
 }
 fn();
 // keyboard ,mouse ,scroll
+
+
 
 
 
