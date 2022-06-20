d2525834b00a85cd9295e6111e23df6df981553c
Jasbir <mr.jasbirsingh96@gmail.com>
2022-06-20 22:12:12 +0530

    added upto wait and click topic

--- Web-march-22/Module_4_Async/lec_5_automation/activity/hk/hackerrank_code_submitter.js
+++ Web-march-22/Module_4_Async/lec_5_automation/activity/hk/hackerrank_code_submitter.js
@@ -9,8 +9,6 @@ const credObj = require("./secrets");
 //     password: "",
 //     email: ""
 // }
-
-
 async function fn() {
     // ***************************************Login********************************************
     const browserRepresentativeObj = await puppeteer.launch({
@@ -18,7 +16,7 @@ async function fn() {
         executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
         defaultViewport: null,
         args: ["--start-maximized", "--start-in-incognito"],
-        slowMo: 100
+        slowMo: 50
     });
     //new  tab open  
     const tab = await browserRepresentativeObj.newPage();
@@ -27,7 +25,9 @@ async function fn() {
     await tab.type("input[type='text']", credObj.email, { delay: 20 });
     await tab.type("input[type='password']", credObj.password, { delay: 20 });
     await tab.keyboard.press("Enter");
-
+    //    promises compose 
+    await waitAndClickTopic("Java", tab);
+    await waitAndClickQuestion("Java Stdin and Stdout I", tab)
     // choose a topic  -> ?? Java
     // select questions -> ??  Java Stdin and Stdout I
     // write the code ->  -> code read type   
@@ -36,6 +36,32 @@ async function fn() {
 }
 fn();
 // keyboard ,mouse ,scroll
+async function waitAndClickTopic(name, tab) {
+    await tab.waitForSelector(".topics-list", { visible: true });
+    // let elems = await tab.$$(".topics-list .topic-card a"); -> document.querySelectorAll
+    // let elems = await tab.$(".topics-list .topic-card a"); 
+    // console.log(elems.length);
+    await tab.evaluate(findAndClick, name);
+    // console.log(idx);
+    function findAndClick(name) {
+        let alltopics = document.querySelectorAll(".topics-list .topic-card a");
+        // return idx
+        let idx;
+        for (idx = 0; idx < alltopics.length; idx++) {
+            let cTopic = alltopics[idx].textContent.trim();
+            console.log(cTopic);
+            if (cTopic == name) {
+                break;
+
+            }
+        }
+        //document  -> elem 
+        alltopics[idx].click();
+        // return idx;
+    }
+
+    // await tab.click(selector);
+}
 
 
 
