ce0c6404d3a440802a3615811720a13216d5e623
Jasbir <mr.jasbirsingh96@gmail.com>
2022-06-27 21:07:28 +0530

    added upto todo.js

--- Web-march-22/Browser/script.js
+++ Web-march-22/Browser/script.js
@@ -5,7 +5,7 @@ let elem = document.querySelector(".next_h1");
 console.log("line number 5", elem);
 // array of element 
 let allh1s = document.querySelectorAll("h1");
-console.log("line number 6", allh1s);
+console.log("line number 8", allh1s);
 // content get 
 // input type of value -> where user set's the data 
 let inputElem = document.querySelector("input");
@@ -15,32 +15,30 @@ let para = document.querySelector("p");
 let text = para.textContent;
 console.log("line number 19", text);
 
-// get attributes 
+// // get attributes 
 let anchorElem = document.querySelector("a");
 let output = anchorElem.getAttribute("href");
 console.log("line number 23", output);
 
-// *****************************create*******************
-// create p tag
+// // *****************************create*******************
+// // create p tag
 let pElem = document.createElement("p");
 // put content
 pElem.textContent = "Hello DOM";
-// elem -> copy
+// // elem -> copy
 let newpElem = pElem.cloneNode(true);
 newpElem.textContent = "append child";
-// place it in the dom tree
+// // place it in the dom tree
 let body = document.querySelector("body");
-// it will add at last
-body.appendChild(newpElem);
-//  before an elem
+// // it will add at last
+body.appendChild(pElem);
+// //  before an elem
 // parent.insertBefore(curreElem,beforethiselem)
-body.insertBefore(pElem, elem);
-// setStyling 
+body.insertBefore(newpElem, elem);
+// // setStyling 
 pElem.style.backgroundColor = "lightgreen";
-
-
-// *****************update************************
-// content
+// // *****************update************************
+// // content
 newpElem.textContent = "modified";
 // styling 
 pElem.style.backgroundColor = "lightblue";
--- Web-march-22/Browser/todo.js
+++ Web-march-22/Browser/todo.js
@@ -1,36 +1,33 @@
-window.addEventListener("load", function () {
-    console.log("Hello");
-    // button
-    let button = document.querySelector("button");
-    // input
-    let input = document.querySelector("input");
-    // ul
-    let ul = document.querySelector("ul");
-    // to listen to an event -> and run our logic after that event
-    button.addEventListener("click", fn);
-    function fn() {
-        let task = input.value;
-        if (task) {
-            taskCreator(task);
 
-            input.value = "";
-        } else {
-            alert("Enter some task");
-        }
-    }
-    function taskCreator(task) {
-        let div = document.createElement("div");
-        let li = document.createElement("li");
-        let button = document.createElement("button");
-        li.textContent = task;
-        button.textContent = "X";
-        div.appendChild(li);
-        div.appendChild(button);
-        ul.appendChild(div);
-        // when button is created ;
-        button.addEventListener("click", function () {
-            div.remove();
-        })
+console.log("Hello");
+// button
+let button = document.querySelector("button");
+// input
+let input = document.querySelector("input");
+// ul
+let ul = document.querySelector("ul");
+// to listen to an event -> and run our logic after that event
+button.addEventListener("click", fn);
+function fn() {
+    let task = input.value;
+    if (task) {
+        taskCreator(task);
+        input.value = "";
+    } else {
+        alert("Enter some task");
     }
 }
-) 
+function taskCreator(task) {
+    let div = document.createElement("div");
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    li.textContent = task;
+    button.textContent = "X";
+    div.appendChild(li);
+    div.appendChild(button);
+    ul.appendChild(div);
+    // when button is created ;
+    button.addEventListener("click", function () {
+        div.remove();
+    })
+}
