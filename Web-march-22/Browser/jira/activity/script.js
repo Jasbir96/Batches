2bc0cb3d642f8fda9e8655455d188d5950aa62cf
Jasbir <mr.jasbirsingh96@gmail.com>
2022-06-27 23:01:33 +0530

    added upto changing color

---
+++ Web-march-22/Browser/jira/activity/script.js
@@ -0,0 +1,17 @@
+let allColors = document.querySelectorAll(".color");
+let main = document.querySelector(".main");
+for (let i = 0; i < allColors.length; i++) {
+    allColors[i].addEventListener("click", changeColor)
+}
+// e-> is the object that will be given by browser
+function changeColor(e) {
+    // e.currentarget -> refers the element on which event has 
+    // occurred
+    let elem = e.currentTarget;
+    // all the classes present on the element 
+    let allclasses = elem.classList;
+    let color = allclasses[1];
+    console.log(color);
+    main.style.backgroundColor = color;
+    // console.log(elem, " ", allclasses);
+}
