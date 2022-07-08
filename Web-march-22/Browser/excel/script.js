1a56fa8419abed13b63c8d0743ff8ac9126ff7ae
Jasbir <mr.jasbirsingh96@gmail.com>
2022-07-08 22:00:48 +0530

    overflow and grid formation

--- Web-march-22/Browser/excel/script.js
+++ Web-march-22/Browser/excel/script.js
@@ -1,5 +1,7 @@
 let topRow = document.querySelector(".top_row");
 let leftCol = document.querySelector(".left_col");
+let grid = document.querySelector(".grid");
+// left column ke cells
 for (let i = 1; i <= 100; i++) {
     // create a cell
     let div = document.createElement("div");
@@ -7,10 +9,23 @@ for (let i = 1; i <= 100; i++) {
     div.textContent = i;
     leftCol.appendChild(div);
 }
+// top row ke cells
 for (let i = 1; i <= 26; i++) {
     // create a cell
     let div = document.createElement("div");
     div.setAttribute("class", "cell");
     div.textContent = String.fromCharCode(i + 64);
     topRow.appendChild(div);
+}
+// grid 
+for (let i = 1; i <= 100; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 1; j <= 26; j++) {
+        let col = document.createElement("div");
+        col.setAttribute("class", "cell");
+        col.textContent = `${String.fromCharCode(64 + j)} - ${i}`
+        row.appendChild(col);
+    }
+    grid.appendChild(row);
 }
