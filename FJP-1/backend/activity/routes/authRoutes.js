53e378ee6e20926de601e0609830289d80cccf16
Jasbir <mr.jasbirsingh96@gmail.com>
2022-07-31 23:43:23 +0530

    ammended the code to update for imports

--- FJP-1/backend/activity/routes/authRoutes.js
+++ FJP-1/backend/activity/routes/authRoutes.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 const authRouter = express.Router();
 const { signupController, loginController,
     resetPasswordController, forgetPasswordController }
--- FJP-1/backend/activity/routes/userRoutes.js
+++ FJP-1/backend/activity/routes/userRoutes.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const userRouter = express.Router();
 const { getAllUsersController, profileController } =
     require('../controller/userController');
