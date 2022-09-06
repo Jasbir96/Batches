3e84a818af98bfd5bb253ad557ec2e460ff44b78
Jasbir <mr.jasbirsingh96@gmail.com>
2022-09-06 22:08:52 +0530

    added login support

--- Web-march-22/react/reels/src/index.js
+++ Web-march-22/react/reels/src/index.js
@@ -10,13 +10,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // steps import 
 // provide through the app -> Provider 
 root.render(
-        <Provider store={reduxStore}>
+        <Provider store={store}>
                 <ReactReduxFirebaseProvider
                 // firebase lib link
                         firebase={firebase}
                         // firebase config
                         config={firebaseConfig}
-                        // dispatch to change the store using reducer 
+ // dispatch to change the store using reducer 
                         dispatch={reduxStore.dispatch}
                 >
                         <App />
--- Web-march-22/react/reels/src/redux/store.js
+++ Web-march-22/react/reels/src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import {  getFirebase } from 'react-redux-firebase';
 import authReducer from './reducer/authReducer';
-const reduxStore = createStore(authReducer,applyMiddleware
-        (thunk.withExtraArgument({ getFirebase })));//redux binding for firebase );
+const reduxStore = createStore(authReducer,
+applyMiddleware
+        (thunk.withExtraArgument({ getFirebase })));
+        //redux binding for firebase );
 export default reduxStore;
 
   
