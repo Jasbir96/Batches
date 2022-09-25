243a6ccd53a86d60c829c1564942639514e9e2e1
Jasbir <mr.jasbirsingh96@gmail.com>
2022-09-25 15:16:41 +0530

    added upto cloning of objects

--- Interview_prep_2022/javascript/4__polyfills/deepclone.js
+++ Interview_prep_2022/javascript/4__polyfills/deepclone.js
@@ -9,7 +9,7 @@ let person = {
         country: 'USA'
     },
     movies: [[{ key: "Die hard" }], "First Avenger"]
-};
+}; 
 
 // you need to make a deep copy for it
 // * shallow copy 
@@ -31,3 +31,28 @@ let parsedString = JSON.parse(string);
 person.firstName = "fake";
 person.address.city = "fake";
 console.log("parsedString", parsedString);
+// console.log(parsedString.movies[0]);
+
+
+// deep clone implementation 
+let superClone = (object) => {
+    let isArr = Array.isArray(object);
+    let cloning = isArr ? [] : {};
+    // [fn,lastName,address]
+    Object.keys(object).map((prop) => {
+        if (Array.isArray(object[prop])) {
+            cloning[prop] = [...object[prop]];
+            for (let i = 0; i < cloning[prop].length; i++) {
+                if (cloning[prop][i] == "object") {
+                    cloning[prop][i] = superClone(object[prop][i]);
+                }
+            }
+            // array -> objects -> super clone 
+        } else if (typeof object[prop] === "object") {
+            cloning[prop] = superClone(object[prop]);
+        } else {
+            cloning[prop] = object[prop];
+        }
+    });
+    return cloning;
+};
