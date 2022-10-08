e05b22acbbbc10ec66ac2814586ea62de299c6a9
Jasbir <mr.jasbirsingh96@gmail.com>
2022-10-08 16:52:27 +0530

    added upto object.create

---
+++ Interview_prep_2022/javascript/6_OOPS_In_JS/object_Creation/create.js
@@ -0,0 +1,21 @@
+let a = {
+    p: 10,
+    sayHi: function () {
+        console.log("Hi p");
+    }
+};
+//  b will be an empty that inherits the properties and methods of it's parent a
+let b = Object.create(a);
+b.prop=20;
+// your own properties nad method can be printed
+console.log(b,a);
+console.log(b.p);
+
+// can a children modify it's parent prop??
+b.p=20;
+console.log("`````````````````````````````");
+console.log(b, a);
+
+// javascript multiple inheritance -> ❌
+// multilevel -> ✔
+
